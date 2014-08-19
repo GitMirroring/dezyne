5c101978dfc80f26ffcd2e3c6a701bc94b0a5614
Rutger van Beusekom <rutger.van.beusekom@verum.com>
2014-08-19 10:16:24 +0200

    gaiag: c++: compileable and runnable output for Alarm.asd

--- language/asd/templates/c3.cc.scm
+++ language/asd/templates/c3.cc.scm
@@ -42,15 +42,32 @@ struct #.model
   : #(map-ports
 #{#.port ()#} (ast:ports model) "\n, ")
   {
-#(map-ports
-#{#(map-port-events
-#{#.port .in.#.event  = asd::bind(&#.model ::#.event , this);
-#} port (filter ast:in? (ast:events port)))
-#} (filter ast:provides? (ast:ports model)))
-#(map-ports
-#{#(map-port-events
-#{#.port .out.#.event  = asd::bind(&#.model ::#.event , this);
-#} port (filter ast:out? (ast:events port)))
-#} (filter ast:requires? (ast:ports model)))
+#(map-ports #{#(map-port-events #{#.port .in.#.event  = asd::bind(&#.model ::#.event , this);
+#} port (filter ast:in? (ast:events port))) #} (filter ast:provides? (ast:ports model)))#
+(map-ports #{#(map-port-events #{#.port .out.#.event  = asd::bind(&#.model ::#.event , this);
+#} port (filter ast:out? (ast:events port))) #} (filter ast:requires? (ast:ports model))) }
+#(map-ports #{#(map-port-events #{void #.event ()
+  {
+    std::cout << "#.model ::#.event " << std::endl;
   }
+#} port (filter ast:in? (ast:events port))) #} (filter ast:provides? (ast:ports model)))#
+(map-ports #{#(map-port-events #{void #.event ()
+  {
+    std::cout << "#.model ::#.event " << std::endl;
+  }
+#} port (filter ast:out? (ast:events port))) #} (filter ast:requires? (ast:ports model)))
 };
+
+template<typename Port>
+void connect(Port& provided, Port& required)
+{
+  provided.out = required.out;
+  required.in = provided.in;
+}
+
+int main()
+{
+  Alarm alarm;
+
+  alarm.console.in.arm();
+}
