f211d5d0b5ac85bfc88f000e204a524feb394865
Jan Nieuwenhuizen <janneke@gnu.org>
2014-09-11 19:33:15 +0200

    Gaiag: gom:map: collapse statements-map into generic full-tree map.

--- gaiag/module/gaiag/gom/map.scm
+++ gaiag/module/gaiag/gom/map.scm
@@ -18,11 +18,8 @@
 (read-set! keywords 'prefix)
 
 (define-module (gaiag gom map)
-  :use-module (ice-9 and-let-star)
   :use-module (ice-9 curried-definitions)
   :use-module (ice-9 pretty-print)
-
-  :use-module (srfi srfi-1)
 
   :use-module (oop goops)
   :use-module (oop goops describe)
@@ -30,94 +27,27 @@
   :use-module (gaiag gom gom)
   :use-module (gaiag gom display)
 
-  :export (map-statements))
+  :export (gom:map))
 
-;;(define-generic map)
-(define-method (gom:map f (o <ast>)) (f o))
+;; (define ((map f) o) (gom:map f o))
+;; (define-generic map)
+;; (define ((ref-slot name) o) (slot-ref o name))
 
-(define-method (gom:map f (o <top>))
-  (or (f o)) o)
+(define-method (gom:map f)
+  (lambda (o) (gom:map f o)))
 
-(define ((gom:map f) o)
-  (gom:map f o))
+(define-method (gom:map f o) (f o))
 
-(define-method (gom:map f (o <root>))
-  (or (f o)
-      (make <root> :elements (map f (.elements o)))))
-
-(define-method (gom:map f (o <interface>))
-  (or (f o)
-      (make <interface>
-        :name (.name o)
-        :types (.types o)
-        :events (.events o)
-        :behaviour (and=> (.behaviour o) (map-statements f)))))
-
-(define-method (gom:map f (o <component>))
-  (or (f o)
-      (make <component>
-        :name (.name o)
-        :ports (.ports o)
-        :behaviour (and=> (.behaviour o) (map-statements f)))))
-
-(define-method (gom:map f (o <behaviour>))
-  (or (f o)
-      (make <behaviour>
-        :name (.name o)
-        :types (.types o)
-        :variables (.variables o)
-        :functions (gom:map f (.functions o))
-        :statement (and=> (.statement o) (map-statements f)))))
-
-(define-method (gom:map f (o <functions>))
-  (or (f o)
-      (make <functions> :elements (map f (.elements o)))))
-
-(define-method (gom:map f (o <function>))
-  (or (f o)
-      (make <function>
-        :name (.name o)
-        :signature (.signature o)
-        :recursive (.recursive o)
-        :statement (gom:map f (.statement o)))))
-
-(define-method (gom:map f (o <if>))
-  (or (f o)
-      (make <if>
-        :expression (.expression o)
-        :then (gom:map f (.then o))
-        :else (and=> (.else o) (map-statements f)))))
-
-(define ((ref-slot name) o) (slot-ref o name))
-
-(define-method (has-method? (o <top>) f)
-  (and-let* (((is-a? f <generic>))
-             (fs (generic-function-methods f))
-             (signatures (map (ref-slot 'specializers) fs)))
-            (find (lambda (x) (equal? x (list (class-of o)))) signatures)))
-
-(define-method (hairy-map-statements (o <compound>) f)
-  (if (has-method? o f)
-      (f o)
-      (or (and (not (is-a? f <generic>))
-               (f o))
-          (make <compound> :elements (map f (.elements o))))))
-
-;; (define-method (gom:map (o <compound>) f)
-;;   (f (make <compound> :elements (map (gom:map f) (.elements o)))))
-
-(define-method (gom:map (o <compound>) f)
-  (or (f o)
-      (make <compound> :elements (map (gom:map f) (.elements o)))))
-
-(define-method (gom:map f (o <guard>))
-  (or (f o)
-      (make <guard>
-        :expression (.expression o)
-        :statement (gom:map f (.statement o)))))
-
-(define-method (gom:map f (o <on>))
-  (or (f o)
-      (make <on>
-        :triggers (.triggers o)
-        :statement (gom:map f (.statement o)))))
+(define-method (gom:map f (o <ast>))
+  (define ((make-initializer o) name)
+    (let ((element (slot-ref o name)))
+      (list (symbol->keyword name)
+            (if (list? element)
+                (map (gom:map f) element)
+                (gom:map f element)))))
+  (f (let* ((class (class-of o))
+            (slots (class-slots class))
+            (names (map slot-definition-name slots))
+            (initializers (map (make-initializer o) names))
+            (arguments (cons class (apply append initializers))))
+       (apply make arguments))))
