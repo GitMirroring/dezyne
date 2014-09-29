ac2afc93b4d3d90a1de0bc95e7bb142a38c0b361
Rutger van Beusekom <rutger.van.beusekom@verum.com>
2014-09-29 12:48:36 +0200

    Revert "fix issue with deep illegals in if"
    This reverts commit 72b486ab50a7e2a1abe2c6ad177b8c5f6e0051fc.

--- gaiag/module/gaiag/csp.scm
+++ gaiag/module/gaiag/csp.scm
@@ -675,7 +675,7 @@
          (pred (if else-illegal? (list 'and '(! IG) pred-then) pred-then)))
     (make <csp-if>
       :context context
-      :expression (make <expression> :value expr)
+      :expression (make <expression> :value pred)
       :then (ast-transform- ast then return context)
       :else (or (ast-transform- ast else return context) '()))))
 
