f16cd915fe37c68202c325c231e7669861f918f8
Jan Nieuwenhuizen <janneke@gnu.org>
2014-10-07 23:57:46 +0200

    Gaiag: c++: template cleanup: use (connect-ports).

--- gaiag/templates/c++/system.cc.scm
+++ gaiag/templates/c++/system.cc.scm
@@ -17,16 +17,7 @@ namespace component
     (map (init-bind model #{ #port(#instance)#})
          (filter bind-port? ((compose .elements .bindings) model))))
 {
- # (map
-    (lambda (bind)
-      (let* ((left (.left bind))
-             (left-port (gom:port model left))
-             (right (.right bind))
-             (provided-required (if (gom:provides? left-port)
-                                    (cons left right)
-                                    (cons right left)))
-             (provided (binding-name model (car provided-required)))
-             (required (binding-name model (cdr provided-required))))
-        (->string (list "connect("provided "," required ");\n"))))
-    (filter (negate bind-port?) ((compose .elements .bindings) model))) }
+ # (map (connect-ports model #{
+    connect(#provided , #required );
+#}) (filter (negate bind-port?) ((compose .elements .bindings) model))) }
 }
