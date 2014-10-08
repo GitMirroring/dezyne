25936a458eb906b96b7193a7accc84c96511bbe3
Rutger van Beusekom <rutger.van.beusekom@verum.com>
2014-10-08 08:29:59 +0200

    gaiag: csp: capture reply reordering in test.

---
+++ gaiag/baseline/component-reply_reorder-c3.cc
@@ -0,0 +1,89 @@
+// Gaiag --- Guile in Asd In Asd in Guile.
+//
+// This file is part of Gaiag.
+//
+// Copyright © 2014 Rutger van Beusekom <rutger.van.beusekom@verum.com>
+//
+// Gaiag is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Gaiag is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Gaiag.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#include "component-reply_reorder-c3.hh"
+
+void handle_event(void*, const asd::function<void()>&);
+
+template <typename R>
+inline asd::function<R()> connect(void*, const asd::function<R()>& event)
+{
+  return event;
+}
+
+template <>
+inline asd::function<void()> connect<void>(void* scope, const asd::function<void()>& event)
+{
+  return asd::bind(handle_event, scope, event);
+}
+
+namespace component
+{
+  reply_reorder::reply_reorder()
+  : first(true)
+  , po_p()
+  , po_r()
+  {
+    po_p.in.start = connect<void>(this, asd::bind<void>(&reply_reorder::po_p_start, this));
+    po_r.out.pong = connect<void>(this, asd::bind<void>(&reply_reorder::po_r_pong, this));
+  }
+
+  void reply_reorder::po_p_start()
+  {
+    std::cout << "reply_reorder.po_p_start" << std::endl;
+    {
+      po_r.in.ping ();
+
+    }
+
+
+  }
+  void reply_reorder::po_r_pong()
+  {
+    std::cout << "reply_reorder.po_r_pong" << std::endl;
+    {
+      if (first)
+
+      {
+        po_p.out.busy ();
+        first = not (first);
+
+      }
+
+      if (not (first))
+
+      {
+        po_p.out.finish ();
+        first = not (first);
+
+      }
+
+
+    }
+
+
+  }
+
+
+
+}
---
+++ gaiag/baseline/interface-Requires-c3.hh
@@ -0,0 +1,53 @@
+// Gaiag --- Guile in Asd In Asd in Guile.
+//
+// This file is part of Gaiag.
+//
+// Copyright © 2014 Rutger van Beusekom <rutger.van.beusekom@verum.com>
+//
+// Gaiag is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Gaiag is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Gaiag.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#ifndef INTERFACE_REQUIRES_C3_HH
+#define INTERFACE_REQUIRES_C3_HH
+
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
+namespace asd
+{
+  using boost::function;
+  using boost::bind;
+}
+
+namespace interface
+{
+  struct Requires
+  {
+
+    struct
+    {
+      asd::function<void()> ping;
+    } in;
+
+    struct
+    {
+      asd::function<void()> pong;
+    } out;
+  };
+}
+
+#endif
