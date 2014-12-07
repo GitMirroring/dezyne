21e390db98a72225ad87cfb35e52beb962f08dff
Rutger van Beusekom <rutger.van.beusekom@verum.com>
2014-12-07 23:29:27 +0100

    webapp: add database.

--- database.sql
+++
@@ -1,3 +0,0 @@
-create table users (name varchar, email varchar, cred varchar(64), salt varchar(64));
-create table events(time timestamp with time zone, command varchar(128), direction varchar(64), sha1 varchar(40), FOREIGN KEY(sha1) REFERENCES files(sha1));
-create table files(sha1 varchar(40) PRIMARY KEY, content text);
