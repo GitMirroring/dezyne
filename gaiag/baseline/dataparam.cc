f9f62f9e91d39a21a14546e5ad3081d37f7e253e
Rutger van Beusekom <rutger.van.beusekom@verum.com>
2015-03-02 16:42:22 +0100

    gaiag: c++: update traces.

---
+++ gaiag/baseline/dataparam.cc
@@ -0,0 +1,114 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2015 Rutger van Beusekom <rutger.van.beusekom@verum.com>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#include "Datasystem.hh"
+
+#include "locator.hh"
+#include "runtime.hh"
+
+#include <iostream>
+
+void a0()
+{
+  std::clog << "a0()" << std::endl;
+}
+
+void a(int i)
+{
+  std::clog << "a(" << i << ")" << std::endl;
+}
+
+void aa(int i, int j)
+{
+  assert(j == 123);
+  std::clog << "aa(" << i << "," << j << ")" << std::endl;
+}
+
+void a6(int i0, int i1, int i2, int i3, int i4, int i5)
+{
+  assert(i0 == 0);
+  assert(i1 == 1);
+  assert(i2 == 2);
+  assert(i3 == 3);
+  assert(i4 == 4);
+  assert(i5 == 5);
+  std::clog << "a6(" << i0 << "," << i1 << "," << i2 << "," << i3 << "," << i4 << "," << i5 << ")" << std::endl;
+}
+
+int main()
+{
+  dezyne::locator l;
+  dezyne::runtime rt;
+  l.set(rt);
+
+  dezyne::Datasystem c(l);
+
+  c.meta.parent = 0;
+  c.meta.name = "c";
+
+  c.port.out.meta.component = "main";
+  c.port.out.meta.port = "port";
+  c.port.out.meta.address = 0;
+
+  c.port.out.a0 = a0;
+  c.port.out.a = a;
+  c.port.out.aa = aa;
+  c.port.out.a6 = a6;
+
+  dezyne::apply(reinterpret_cast<dezyne::component*>(&c), [](const dezyne::meta& m){
+      std::clog << m.parent << " " << m.address << " " << m.name << std::endl;
+    });
+
+  assert(dezyne::IDataparam::Status::Yes == c.port.in.e0r());
+  c.port.in.e0();
+  assert(dezyne::IDataparam::Status::Yes == c.port.in.er(123));
+  c.port.in.e(123);
+  assert(dezyne::IDataparam::Status::No == c.port.in.eer(123,345));
+
+  int i = 0;
+  c.port.in.eo(i);
+  assert(i == 234);
+
+  int j = 0;
+  c.port.in.eoo(i,j);
+  assert(i == 123 && j == 456);
+
+  c.port.in.eio(i,j);
+  assert(i == 123 && j == i);
+
+  c.port.in.eio2(i);
+  assert(i == 246);
+
+
+  assert(dezyne::IDataparam::Status::Yes == c.port.in.eor(i));
+  assert(i == 234);
+
+  assert(dezyne::IDataparam::Status::Yes == c.port.in.eoor(i,j));
+  assert(i == 123 && j == 456);
+
+  assert(dezyne::IDataparam::Status::Yes == c.port.in.eior(i,j));
+  assert(i == 123 && j == i);
+
+  assert(dezyne::IDataparam::Status::Yes == c.port.in.eio2r(i));
+  assert(i == 246);
+}
