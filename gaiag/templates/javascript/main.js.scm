fef58c2d3b58af5337dce1d34dfd1b750f7964ec
Jan Nieuwenhuizen <janneke@gnu.org>
2015-04-09 16:52:35 +0200

    gaiag: javascript: cleanup fill_event_map. Fixes code trace equivalence.

--- gaiag/templates/javascript/main.js.scm
+++ gaiag/templates/javascript/main.js.scm
@@ -1,33 +1,23 @@
+function #.model _fill_event_map(m, e) {
 #(map
-  (lambda (model)
-  (append
-   `("function " ,(.name model) "_fill_event_map(m, e)\n{\n")
-   (if (is-a? model <component>)
-      (map
-       (lambda (port)
-       (map (define-on model port #{
-          if (!m.#port .#direction .#event) {
-            m.#port .#direction .#event  = function () {console.error('#port .#direction .#event ');};
-         }
-         if (!e['#port .#event ']) {
+    (lambda (port)
+     (map (define-on model port #{
+        m.#port .#direction .#event  = function () {console.error('#port .#direction .#event ');};
+#}) (filter (negate (gom:dir-matches? port))
+       (gom:events port)))) (gom:ports model))#
+(map
+    (lambda (port)
+    (map (define-on model port #{
            e['#port .#event '] = m.#port .#direction .#event;
-         }
-#}) (gom:events port))) (delete-duplicates (gom:ports model)))
-    '())
-    '("}\n")))
-  (if (is-a? model <component>) (list model) (delete-duplicates (map (lambda (i) (gom:import (.component i))) (.elements (.instances model))))))
-  
+#}) (filter (gom:dir-matches? port)
+       (gom:events port)))) (gom:ports model)) }
+
 function main () {
   var rt = new dezyne.runtime ();
   var event_map = {};
   var sut = new dezyne.#.model (rt, {name: 'sut'});
   
-  #(string-if (is-a? model <component>)
-              #{#.model _fill_event_map(sut, event_map); #}
-              (->string
-              (map
-                (lambda (i)
-                   `(,(.component i) "_fill_event_map(sut." ,(.name i) ", event_map);\n")) (.elements (.instances model)))))
+  #.model _fill_event_map(sut, event_map);
 
   var readline = require ('readline');
   var rl = readline.createInterface ({
