9a70c0d8973e87e41f120d32a10b75b034c090b6
Jan Nieuwenhuizen <janneke@gnu.org>
2015-04-09 16:47:50 +0200

    gaiag: python: cleanup fill_event_map. Fixes code trace equivalence.

--- gaiag/templates/python/main.py.scm
+++ gaiag/templates/python/main.py.scm
@@ -7,34 +7,26 @@ sys.path.insert (0, os.path.dirname (sys.argv[0]))
 import dezyne.#.model
 import runtime
 
+def #.model _fill_event_map (m, e):
 #(map
-  (lambda (model)
-  (append
-   `("def " ,(.name model) "_fill_event_map (m, e):\n")
-   (if (is-a? model <component>)
-      (map
-       (lambda (port)
-       (map (define-on model port #{
-    if (not m.#port .#direction s.#event):
-        m.#port .#direction s.#event  = lambda *args: sys.stderr.write ('#port .#direction .#event \n')
-    if ('#port .#event ' not in e.keys ()):
-        e['#port .#event '] = m.#port .#direction s.#event
-#}) (gom:events port))) (delete-duplicates (gom:ports model)))
-    '("    pass"))
-    '("\n")))
-  (if (is-a? model <component>) (list model) (delete-duplicates (map (lambda (i) (gom:import (.component i))) (.elements (.instances model))))))
+    (lambda (port)
+    (map (define-on model port #{
+    m.#port .#direction s.#event  = lambda *args: sys.stderr.write ('#port .#direction .#event \n')
+#}) (filter (negate (gom:dir-matches? port))
+       (gom:events port)))) (gom:ports model))#
+(map
+    (lambda (port)
+    (map (define-on model port #{
+    e['#port .#event '] = m.#port .#direction s.#event
+#}) (filter (gom:dir-matches? port)
+       (gom:events port)))) (gom:ports model))
 def main ():
     rt = runtime.runtime ()
     event_map = {}
     sut = dezyne.#.model  (rt, name='sut')
 
-#(string-if (is-a? model <component>)
-#{
-    #.model _fill_event_map (sut, event_map)#}
- (->string
- (map
- (lambda (i)
- `("    " ,(.component i) "_fill_event_map (sut." ,(.name i) ", event_map)\n")) (.elements (.instances model)))))
+    #.model _fill_event_map (sut, event_map)
+
     event = sys.stdin.readline ().strip ()
     while event:
         if event in event_map.keys ():
