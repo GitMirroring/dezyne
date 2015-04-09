caf805993b4d8b00f5788bac68b71a94ac5c6a07
Jan Nieuwenhuizen <janneke@gnu.org>
2015-04-09 15:43:36 +0200

    gaiag: c++: cleanup fill_event_map.

--- gaiag/templates/c++/main.cc.scm
+++ gaiag/templates/c++/main.cc.scm
@@ -10,22 +10,23 @@ namespace dezyne
 {
   typedef std::map<std::string, std::function<void()>> event_map;
 
-#(map
-  (lambda (model)
-  (append
-   `("void fill_event_map(" ,(.name model) "& m, event_map& e)\n{\nint dzn_i = 0;\n")
-   (if (is-a? model <component>)
-      (map
-       (lambda (port)
-       (map (define-on model port #{
-          if (not m.#port .#direction .#event) {
-            m.#port .#direction .#event  = [] (#parameters) {std::clog << "#port .#direction .#event " << std::endl;#(string-if (not (eq? return-type 'void)) #{ return (#return-type)0;#})};
-         }
-         if (e.find("#port .#event ") == e.end()) e["#port .#event "] = #(string-if (null? argument-list) #{m.#port .#direction .#event; #} #{ [m,&dzn_i] {m.#port .#direction .#event (#(comma-join (map (lambda (i) "dzn_i") argument-list)));};#})
-#}) (gom:events port))) (delete-duplicates (gom:ports model)))
-    '())
-    '("}\n")))
-  (if (is-a? model <component>) (list model) (delete-duplicates (map (lambda (i) (gom:import (.component i))) (.elements (.instances model)))))) }
+
+void fill_event_map(#.model & m, event_map& e)
+{
+  int dzn_i = 0;
+  #(map
+    (lambda (port)
+    (map (define-on model port #{
+      m.#port .#direction .#event  = [] (#parameters) {std::clog << "#port .#direction .#event " << std::endl;#(string-if (not (eq? return-type 'void)) #{ return (#return-type)0;#})};
+#}) (filter (negate (gom:dir-matches? port))
+       (gom:events port)))) (gom:ports model))
+  #(map
+    (lambda (port)
+    (map (define-on model port #{
+       e["#port .#event "] = #(string-if (null? argument-list) #{m.#port .#direction .#event; #} #{ [m,&dzn_i] {m.#port .#direction .#event (#(comma-join (map (lambda (i) "dzn_i") argument-list)));};#})
+#}) (filter (gom:dir-matches? port)
+       (gom:events port)))) (gom:ports model)) }
+}
 
 int main()
 {
@@ -37,12 +38,8 @@ int main()
   dezyne::#.model  sut(l);
   sut.dzn_meta.name = "sut";
 
-  #(string-if (is-a? model <component>)
-              #{dezyne::fill_event_map(sut, event_map); #}
-              (->string
-              (map
-                (lambda (i)
-                   `("dezyne::fill_event_map(sut." ,(.name i) ", event_map);\n")) (.elements (.instances model)))))
+  dezyne::fill_event_map(sut, event_map);
+
   sut.check_bindings();
   sut.dump_tree();
 
