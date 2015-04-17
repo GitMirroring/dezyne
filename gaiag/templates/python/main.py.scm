06ffc62b44a835484f5fa353df3466e0cb05ac59
Jan Nieuwenhuizen <janneke@gnu.org>
2015-04-18 00:50:49 +0200

    gaiag: python: main: log returns, read reply values.

--- gaiag/templates/python/main.py.scm
+++ gaiag/templates/python/main.py.scm
@@ -7,11 +7,37 @@ sys.path.insert (0, os.path.dirname (sys.argv[0]))
 import dezyne.#.model
 import runtime
 
+def drop_prefix (string, prefix):
+    if string.startswith (prefix):
+        return string[len(prefix):]
+    return string
+
+def log_void (prefix, event):
+    sys.stderr.write (prefix + event + '\n')
+    sys.stderr.write (prefix + 'return' + '\n')    
+
+def get_value (string_to_value):
+    while True:
+        s = sys.stdin.readline ().strip ()
+        if not s:
+            return 0
+        r = string_to_value (s)
+        if (r != None):
+            return r
+
+def log_valued (prefix, event, string_to_value, value_to_string):
+    sys.stderr.write (prefix + event + '\n')
+    r = get_value (string_to_value)
+    if (r != None):
+        sys.stderr.write (prefix + value_to_string[r] + '\n')
+        return r
+    return 0
+
 def #.model _fill_event_map (m):
 #(map
     (lambda (port)
     (map (define-on model port #{
-    m.#port .#direction port.#event  = lambda *args: sys.stderr.write ('#port .#direction .#event \n') or 0
+    m.#port .#direction port.#event  = lambda *args: #(string-if (eq? return-type 'void) #{log_void ('#port .#direction .', '#event ')#}#{log_valued ('#port .#direction .', '#event ', lambda s: dezyne.#interface .#reply-name .__dict__.get (drop_prefix(s, '#port .#reply-name _'), None), dezyne.#interface .#reply-name _to_string)#})
 #}) (filter (negate (gom:dir-matches? port))
        (gom:events port)))) (gom:ports model))     return {
 #(map
