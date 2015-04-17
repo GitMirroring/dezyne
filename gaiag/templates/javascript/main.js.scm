2dbb7204e673ee2fceb3c416ef20888933daabc1
Jan Nieuwenhuizen <janneke@gnu.org>
2015-04-18 00:16:54 +0200

    gaiag: javascript: main: log returns, read reply values.

--- gaiag/templates/javascript/main.js.scm
+++ gaiag/templates/javascript/main.js.scm
@@ -1,8 +1,32 @@
+var lines = [];
+function read_line () {
+  if (lines.length) {
+    return lines.pop();
+  }
+  process.exit (0);
+}
+
+// function read_line () {
+//   var readline = require ('readline-sync');
+//   var s;
+//   if (s = readline.question ()) {
+//     return s;
+//   }
+//   process.exit (0);
+// }
+
+function drop_prefix(string, prefix) {
+  if (string.indexOf(prefix) === 0) {
+    return string.slice(prefix.length);
+  }
+  return string;
+}
+
 function #.model _fill_event_map(m) {
 #(map
     (lambda (port)
      (map (define-on model port #{
-        m.#port .#direction .#event  = function() {console.error('#port .#direction .#event '); return 0;};
+       m.#port .#direction .#event  = function() {console.error('#port .#direction .#event '); #(string-if (and (eq? return-type 'void) (eq? direction 'in)) #{console.error('#port .#direction .return');#}) #(string-if (not (eq? return-type 'void)) #{var s; while (s = read_line ()) {var r = new dezyne.#interface().#reply-name[drop_prefix(s,'#port .#reply-name _')]; if (r !== undefined) {console.error('#port .#direction .' + new dezyne.#interface().#reply-name _to_string[r]); return r;};}#})};
 #}) (filter (negate (gom:dir-matches? port))
        (gom:events port)))) (gom:ports model))   return {
 #(map
@@ -19,17 +43,14 @@ function main () {
   
   var event_map = #.model _fill_event_map(sut);
 
-  var readline = require('readline');
-  var rl = readline.createInterface ({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  rl.on('line', function(event) {
+  var fs = require ('fs');
+  lines = fs.readFileSync ('/dev/stdin', 'ascii').toString().trim().split ('\n').reverse ();
+  var event;
+  while (event = read_line ()) {
     if (event_map[event]) {
       event_map[event]();
     }
-  });
+  }
 }
 
 main ();
