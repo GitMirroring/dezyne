705eab962287d31a59182eb9068f23958ac2ae96
Jan Nieuwenhuizen <janneke@gnu.org>
2015-04-18 08:09:24 +0200

    gaiag: javascript: main: refactor.

--- gaiag/templates/javascript/main.js.scm
+++ gaiag/templates/javascript/main.js.scm
@@ -1,19 +1,10 @@
 var lines = [];
-function read_line () {
+function read_line() {
   if (lines.length) {
     return lines.pop();
   }
-  process.exit (0);
+  process.exit(0);
 }
-
-// function read_line () {
-//   var readline = require ('readline-sync');
-//   var s;
-//   if (s = readline.question ()) {
-//     return s;
-//   }
-//   process.exit (0);
-// }
 
 function drop_prefix(string, prefix) {
   if (string.indexOf(prefix) === 0) {
@@ -22,11 +13,36 @@ function drop_prefix(string, prefix) {
   return string;
 }
 
+function get_value(string_to_value) {
+  var s;
+  while (s = read_line()) {
+    var r = string_to_value(s);
+    if (r !== undefined) {
+      return r;
+    }
+  }
+}
+
+function log_void(prefix, event) {
+  console.error(prefix + event);
+  console.error(prefix + 'return');
+}
+
+function log_valued(prefix, event, string_to_value, value_to_string) {
+  console.error(prefix + event);
+  var r = get_value(string_to_value);
+  if (r !== undefined) {
+     console.error(prefix + value_to_string[r]);
+     return r;
+  }
+  return 0;
+}
+
 function #.model _fill_event_map(m) {
 #(map
     (lambda (port)
      (map (define-on model port #{
-       m.#port .#direction .#event  = function() {console.error('#port .#direction .#event '); #(string-if (and (eq? return-type 'void) (eq? direction 'in)) #{console.error('#port .#direction .return');#}) #(string-if (not (eq? return-type 'void)) #{var s; while (s = read_line ()) {var r = new dezyne.#interface().#reply-name[drop_prefix(s,'#port .#reply-name _')]; if (r !== undefined) {console.error('#port .#direction .' + new dezyne.#interface().#reply-name _to_string[r]); return r;};}#})};
+       m.#port .#direction .#event  = function() {#(string-if (eq? return-type 'void) #{log_void('#port .#direction .', '#event ');#}#{return log_valued('#port .#direction .', '#event ', function(s) {return new dezyne.#interface().#reply-name[drop_prefix(s, '#port .#reply-name _')];}, new dezyne.#interface().#reply-name _to_string)#})};
 #}) (filter (negate (gom:dir-matches? port))
        (gom:events port)))) (gom:ports model))   return {
 #(map
