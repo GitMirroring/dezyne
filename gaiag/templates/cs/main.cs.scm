79d1e0e4faa1befad692b1f0cd819e9d07d62190
Jan Nieuwenhuizen <janneke@gnu.org>
2015-05-05 20:33:41 +0200

    gaiag: cs: main: valued replies.

--- gaiag/templates/cs/main.cs.scm
+++ gaiag/templates/cs/main.cs.scm
@@ -24,16 +24,63 @@
 using System;
 using System.Collections.Generic;
 
-class EventMap : Dictionary<String, Action> {};
-
 class main {
-                        
+
+  static String drop_prefix(String str, String prefix) {
+    if (str.StartsWith(prefix)) {
+      return str.Substring(prefix.Length);
+    }
+    return str;
+  }
+
+  static void log_in(String prefix, String e) {
+    System.Console.Error.WriteLine(prefix + e);
+    System.Console.Error.WriteLine(prefix + "return");
+  }
+
+  static void log_out(String prefix, String e) {
+    System.Console.Error.WriteLine(prefix + e);
+  }
+
+  static R? string_to_value<R>(String s) where R: struct, IComparable, IConvertible, IFormattable {
+    foreach (R e in Enum.GetValues(typeof(R))) {
+      if (e.ToString().Equals(s)) {
+        return e;
+      }
+    }
+    return null;
+  }
+
+  static R? get_value<R>(String prefix) where R: struct, IComparable, IConvertible, IFormattable {
+    String s;
+    while ((s = System.Console.ReadLine()) != null) {
+      R? r = string_to_value<R>(drop_prefix(s, prefix));
+      if (r != null) {
+        return r;
+        }
+    }
+    Environment.Exit(0);
+    return null;
+  }
+
+  static R log_valued<R>(String prefix, String e, String event_prefix) where R: struct, IComparable, IConvertible, IFormattable {
+    System.Console.Error.WriteLine(prefix + e);
+    R? r = get_value<R>(event_prefix);
+    if (r != null) {
+      System.Console.Error.WriteLine(prefix + typeof(R).Name + "_" + r.ToString());
+    }
+    return default(R);
+  }
+
+
+  private class EventMap : Dictionary<String, Action> {};
+
   private static EventMap fillEventMap(#.model  m) {
   V<int> v = new V<int> (0);
 #(map
     (lambda (port)
     (map (define-on model port #{
-    m.#port .#direction port.#event  = (#parameters) => {System.Console.Error.WriteLine("#port .#direction .#event");#(string-if (not (eq? return-type 'void)) #{ return new #return-type();#})};
+    m.#port .#direction port.#event  = (#parameters) => {#(string-if (eq? return-type 'void) #{log_#direction("#port .#direction .", "#event ");#}#{return log_valued<#interface .#reply-name >("#port .#direction .", "#event ", "#port .#reply-name _");#})};
 #}) (filter (negate (gom:dir-matches? port))
        (gom:events port)))) (gom:ports model))     EventMap e = new EventMap();
 #(map
