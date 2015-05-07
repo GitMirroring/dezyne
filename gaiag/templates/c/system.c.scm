ae58fed0ff56f2b26ad83a93e12ba2522b676fc2
Jan Nieuwenhuizen <janneke@gnu.org>
2015-05-08 01:16:17 +0200

    gaiag: c: cleanup injection.

--- gaiag/templates/c/system.c.scm
+++ gaiag/templates/c/system.c.scm
@@ -9,16 +9,22 @@
   }
 
 void #.model _init(#.model  *self, locator* dezyne_locator, dzn_meta_t* dzn_meta) {
-#((->join ";\n")
-    (append
-            (list (if (pair? (injected-bindings model))
-                      (list "locator* local_locator = locator_clone(dezyne_locator);\n" (map (lambda (binding) (list "locator_set(local_locator, \"" (injected-instance-interface model binding) "\", &self->" (binding-name model (injected-binding binding)) "_)")) (injected-bindings model)))))
-            (list "memcpy(&self->dzn_meta, dzn_meta, sizeof(dzn_meta_t))")
-            (map (init-instance #{ dzn_meta_t dzn_m_#name  = {"#name ", self};
-  #component _init(&self->#name , #(string-if (pair? (injected-bindings model)) #{local_locator#} #{dezyne_locator#}), &dzn_m_#name)#})
-                 (.elements (.instances model)))
-            (map (init-bind model #{ self->#port  = self->#instance #})
-             (filter bind-port? (filter (negate injected-binding?) ((compose .elements .bindings) model))))));
+   memcpy(&self->dzn_meta, dzn_meta, sizeof(dzn_meta_t));
+#(map (init-instance #{
+  dzn_meta_t dzn_m_#name  = {"#name ", self};
+  #component _init(&self->#name , dezyne_locator, &dzn_m_#name);#})
+  (injected-instances model))#
+(string-if (pair? (injected-bindings model)) #{#'()
+   dezyne_locator = locator_clone(dezyne_locator);#})#
+    (map (init-bind model #{#'()
+    locator_set(dezyne_locator, "#interface ", &self->#instance _);#}) (injected-bindings model))#
+(map (init-instance #{#'()
+  dzn_meta_t dzn_m_#name  = {"#name ", self};
+  #component _init(&self->#name , dezyne_locator, &dzn_m_#name);#})
+   (non-injected-instances model))#
+(map (init-bind model #{#'()
+  self->#port  = self->#instance; #})
+    (filter bind-port? (filter (negate injected-binding?) ((compose .elements .bindings) model))))
  # (map (connect-ports model #{
     CONNECT(self->#provided , self->#required );
 #}) (filter (negate bind-port?) ((compose .elements .bindings) model))) }
