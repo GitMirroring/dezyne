77fead2bd97be43415626507d50536b8d124abaa
Jan Nieuwenhuizen <janneke@gnu.org>
2015-09-10 00:18:50 +0200

    gaiag: coroutine: add yield-to and naive scheduler.

--- gaiag/module/gaiag/coroutine.scm
+++ gaiag/module/gaiag/coroutine.scm
@@ -29,15 +29,16 @@
   :use-module (ice-9 optargs)
 
   :use-module (gaiag misc)
-  :export (coroutine))
+  :export (coroutine coroutine-to))
 
 (define-syntax-rule (let/cc var body ...)
   (call/cc (lambda (var) body ...)))
 
-(define-syntax-rule (yield-cc+data cc+data data item)
-  (begin
-    (set! cc+data (let/cc cc ((car cc+data) (cons cc item))))
-    (set! data (cdr cc+data))))
+(define-syntax-rule (yield-cc+data cc+data item)
+  (set! cc+data (let/cc cc ((car cc+data) (cons cc item)))))
+
+(define-syntax-rule (yield-to-cc+data cc+data yield item)
+  (set! cc+data (let/cc cc (yield (cons cc item)))))
 
 (define (producer name work)
   (lambda (yield data)
@@ -52,8 +53,8 @@
 (define* (coroutine routine :optional (start 'start))
   (lambda (cc+data)
     (or (pair? cc+data) (set! cc+data (cons cc+data start)))
-    (letrec ((data (lambda () (cdr cc+data)))
-             (yield (lambda (item) (yield-cc+data cc+data data item))))
+    (let ((data (lambda () (cdr cc+data)))
+          (yield (lambda (item) (yield-cc+data cc+data item))))
       (routine yield data))))
 
 (define (consumer yield data)
@@ -61,7 +62,32 @@
     (stderr "  consumer: ~a\n" (data))
     (yield #f)))
 
+(define (rotate lst)
+  (if (null? lst) '()
+      (append (cdr lst) ((compose list car) lst))))
+
+(define (scheduler routines)
+  (lambda (yield-to cc data)
+    (let loop ((routines (map (compose list coroutine) routines)))
+      (if (null? routines)
+          'scheduler-done
+          (let ((routine (car routines)))
+            (stderr "switch [~a]\n" (data))
+            (yield-to (car routine) (data))
+            (if (not (procedure? (cc))) 'scheduler-done-2
+                (loop (append (cdr routines) (list (cons (cc) (data)))))))))))
+
+(define* (coroutine-to routine :optional (start 'start))
+  (lambda* (:optional (cc+data #f))
+    (or (pair? cc+data) (set! cc+data (cons cc+data start)))
+    (let ((data (lambda () (cdr cc+data)))
+          (cc (lambda () (and (pair? cc+data) (car cc+data))))
+          (yield-to (lambda (cc item) (yield-to-cc+data cc+data cc item))))
+      (routine yield-to cc data))))
+
 (define (ast-> ast)
   ((coroutine (producer 'producer '(a b c))) (coroutine (producer "  consumer" '(0 1 2 3 4 5))))
   (stderr "\n\n")
-  ((coroutine (producer 'producer '(a b c))) (coroutine consumer)))
+  ((coroutine (producer 'producer '(a b c))) (coroutine consumer))
+  (stderr "\n\n")
+  ((coroutine-to (scheduler (list (producer 'ping '(0 1 2)) (producer 'pong '(a b c)))))))
