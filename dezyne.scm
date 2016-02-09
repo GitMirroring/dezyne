5ac38203d5ef79611868f62a1e0774ac28fa024a
Jan Nieuwenhuizen <janneke@gnu.org>
2016-02-09 18:03:29 +0100

    client/daemon: standardize messages further, use daemon_api, websockets:
    client:
    
        {post:{
          command: {
    	name: string,
    	options: {},
    	flles: [],
          },
          auth: {
    	user: string,
    	password: string,
    	ticket: string,
          },
          client: {
    	name: 'dzn',
    	version: string,
          },
          url: string,  // 'http://localhost:3000',
          debug: false|true
        }
        ,
        read:{name:string,content:string},
       }
    
    daemon:
       {
         post:{
          auth: {ticket:ticket}
          type: error|install|progress|result
          command:{
    	name: string
    	status: int
    	stderr: string
    	stdout: string
    	files: []
          }}
         ,
         read:{name:string},
       }

--- dezyne.scm
+++ dezyne.scm
@@ -21,6 +21,13 @@
 ;;; Code:
 
 (define-module (dezyne)
+  #:use-module (ice-9 optargs)
+
+  #:use-module (srfi srfi-1)
+  #:use-module (guix build utils)
+  #:use-module (guix utils)
+
+  
   #:use-module (guix licenses)
 
   #:use-module (guix packages)
@@ -30,6 +37,7 @@
   #:use-module (guix build-system trivial)
   #:use-module (guix utils)
 
+  #:use-module (gnu packages)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages bison)
@@ -37,14 +45,23 @@
   #:use-module (gnu packages ccache)  
   #:use-module (gnu packages compression)
   #:use-module (gnu packages databases)
+  #:use-module (gnu packages graphviz)  
   #:use-module (gnu packages guile)
+  #:use-module (gnu packages gtk)  
   #:use-module (gnu packages flex)
   #:use-module (gnu packages java)  
+  #:use-module (gnu packages linux)
   #:use-module (gnu packages markdown)
   #:use-module (gnu packages node)
+  #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages tcl)
   #:use-module (gnu packages webkit)
   #:use-module (gnu packages xml)
+  #:use-module (gnu packages w3m)
+
+  #:export (patch-source-shebangs-no-symlinks)
   )
 
 (define-public guile-lib-next
@@ -58,6 +75,79 @@
      (append (substitute-keyword-arguments
                  `(#:tests? #f
                             ,@(package-arguments guile-lib)))))))
+
+(define-public tcllib
+  (package
+    (name "tcllib")
+    (version "1.18")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://sourceforge/" name "/"
+                                  name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "05dmrk9qsryah2n17z6z85dj9l9lfyvnsd7faw0p9bs1pp5pwrkj"))))
+    (build-system gnu-build-system)
+    (native-inputs
+     `(("tcl" ,tcl)))
+    (native-search-paths
+     (list (search-path-specification
+	    (variable "TCLLIBPATH")
+	    (separator " ")
+	    (files (list (string-append "lib/tcllib" version ""))))))
+    (home-page "https://core.tcl.tk/tcllib/home")
+    (synopsis "Standard Tcl Library")
+    (description "Tcllib, the standard Tcl library, is a collection of common utility
+ functions and modules all written in high-level Tcl.
+")
+    (license (non-copyleft "http://www.tcl.tk/software/tcltk/license.html"
+			   "Tcl/Tk license"))))
+
+
+(define-public tclxml
+  (package
+    (name "tclxml")
+    (version "3.2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://sourceforge/" name "/"
+                                  name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0ffb4aw63inig3aql33g4pk0kjk14dv238anp1scwjdjh1k6n4gl"))
+	      (patches (list (search-patch (string-append (getenv "HOME") "/development.git/tclxml-3.2-install.patch"))))))
+    (build-system gnu-build-system)
+    (native-inputs
+     `(("tcl" ,tcl)
+       ("tcllib" ,tcllib)
+       ("libxml2" ,libxml2)
+       ("libxslt" ,libxslt)))
+   (native-search-paths
+    (list (search-path-specification
+           (variable "TCLLIBPATH")
+	   (separator " ")
+	   (files (list (string-append "lib/Tclxml" version))))))
+   (arguments
+    `(#:configure-flags
+      (list (string-append "--with-tclconfig="
+			   (assoc-ref %build-inputs "tcl")
+			   "/lib")
+	    (string-append "--with-xml2-config="
+			   (assoc-ref %build-inputs "libxml2")
+			   "/bin/xml2-config")
+	    (string-append "--with-xslt-config="
+			   (assoc-ref %build-inputs "libxslt")
+			   "/bin/xslt-config"))
+      #:phases (modify-phases %standard-phases
+                 (delete 'check))))
+    (home-page "http://tclxml.sourceforge.net/")
+    (synopsis " Tcl library for XML parsing")
+    (description " TclXML provides event-based parsing of XML documents.  The
+ application may register callback scripts for certain document
+ features, and when the parser encounters those features while parsing
+ the document the callback is evaluated.")
+    (license (non-copyleft "http://www.tcl.tk/software/tcltk/license.html"
+			   "Tcl/Tk license"))))
 
 (define-public fdr2
   (package
@@ -103,64 +193,108 @@
 	      "https://www.cs.ox.ac.uk/projects/concurrency-tools/"
 	       "academic"))))
 
+(define* (configure #:key outputs make-maker? #:allow-other-keys)
+  "Configure the given Perl package."
+  (let* ((out (assoc-ref outputs "out")))
+    (zero? (apply system* "sh"))))
+
+(define (file-is-symlink? file)
+  (and (file-exists? file)
+       (eq? 'symlink (stat:type (lstat file)))))
+
+(define* (patch-source-shebangs-no-symlinks #:key source #:allow-other-keys)
+  "Patch shebangs in all source files; this includes non-executable
+files such as `.in' templates.  Most scripts honor $SHELL and
+$CONFIG_SHELL, but some don't, such as `mkinstalldirs' or Automake's
+`missing' script."
+  (for-each patch-shebang
+            (remove (lambda (file)
+                      (or (not (file-exists? file)) ;dangling symlink
+                          (file-is-symlink? file)
+                          (file-is-directory? file)))
+                    (find-files "."))))
+
 (define-public dezyne-server
   (package
     (name "dezyne-server")
-    (version "1.2.2") ;; TODO
+    (version "1.2.2")
     (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    ;;(url "git://git.oban:~buildmaster/development.git")
-		    (url "git://localhost/home/janneke/development.git")
-                    (commit "master")
-		    ;;(branch "master")
-		    ))
+              (method url-fetch)
+              (uri (string-append
+    		    "http://git.oban/" name "-" version ".tar.xz"))
               (sha256
                (base32
-                ;;"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-		"0s1jsfi4dpig5qcjvqp187yzxym80blbwqpf1b8llmwnivqb860c"
-		))))
+    		"17jnbg5myxvw6i470i4w08fj2yd66j7phhkzigidn92g9zly5882"))))
     (build-system gnu-build-system)
     (inputs `(
 	      ("bash" ,bash)
 	      ("boost" ,boost)
-	      ("guile" ,guile-next)
 	      ("expat" ,expat)
 	      ("fdr2" ,fdr2)
+	      ("graphviz" ,graphviz)	      
+	      ("guile" ,guile-next)
+	      ("guile-lib" ,guile-lib-next)
+	      ("gtkmm" ,gtkmm)
 	      ("java" ,icedtea-7)
 	      ("node" ,node)
-	      ("guile-lib" ,guile-lib-next)
-	      ;;("postgresql" ,postgresql)
+	      ("postgresql" ,postgresql)
+	      ("python" ,python-2)
 	      ("webkitgtk-gtk2" ,webkitgtk/gtk+-2)
 	      ))
 
     (native-inputs `(
+		     ("bash" ,bash)
 		     ("bison" ,bison)
-		     ("ccache" ,ccache)
 		     ("guile" ,guile-next)
+		     ("guile-lib" ,guile-lib-next)		     
 		     ("flex" ,flex)
 		     ("jdk" ,icedtea-7 "jdk")
 		     ("markdown" ,markdown)
 		     ("node" ,node)
-		     ;;("postgresql" ,postgresql)
+		     ("perl" ,perl)
 		     ("pkgconfig" ,pkg-config)
+		     ("postgresql" ,postgresql)
+		     ("procps" ,procps)
+		     ("python" ,python-2)
+		     ("tcl" ,tcl)
+		     ("tcllib" ,tcllib)
+		     ("tclxml" ,tclxml)
+		     ("w3m" ,w3m)
 		     ))
 
 
     (outputs '("out" "debug"))
 
-    (arguments `(#:phases (alist-delete 'install %standard-phases)))
-
-    (native-search-paths
-     '()
-     ;; (list (search-path-specification
-     ;;        (variable "GUILE_LOAD_PATH")
-     ;;        (files '("share/guile/site/2.0")))
-     ;;       (search-path-specification
-     ;;        (variable "GUILE_LOAD_COMPILED_PATH")
-     ;;        (files '("lib/guile/2.0/ccache"
-     ;;                 "share/guile/site/2.0"))))
-     )
+    (arguments `(#:modules ((srfi srfi-1)
+			    ,@%gnu-build-system-modules)
+		 #:phases
+		 (modify-phases %standard-phases
+		   (delete 'install)
+		   (add-before
+		    'configure 'setenv
+		    (lambda _
+		      (setenv "TCLLIBPATH"
+			      (string-append (assoc-ref %build-inputs "tcllib")
+					     "/lib/tcllib1.18 "
+					     (assoc-ref %build-inputs "tclxml")
+					     "/lib/Tclxml3.2 "
+					     (getenv "TCLLIBPATH")))
+		      (setenv "GUILE_AUTO_COMPILE" "0")))
+		   (replace 'patch-source-shebangs
+		   	    ;;patch-source-shebangs-no-symlinks
+		   	    (lambda* (#:key outputs #:allow-other-keys)
+		   	      (for-each patch-shebang
+		   	    		(remove (lambda (file)
+		   	    			  (or (not (file-exists? file)) ;dangling symlink
+		   	    			      ;;(file-is-symlink? file)
+						      (and (file-exists? file)
+							   (eq? 'symlink (stat:type (lstat file))))
+		   	    			      (file-is-directory? file)))
+		   	    			(find-files ".")))))
+		   (replace 'configure
+			    (lambda* (#:key outputs #:allow-other-keys)
+			      (let* ((out (assoc-ref outputs "out")))
+				(zero? (system* "./configure"))))))))
 
     (synopsis "Dezyne")
     (description "boo")
