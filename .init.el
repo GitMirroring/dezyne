3783e9aab20287bf5450395d6d3834910d6fae87
Jan Nieuwenhuizen <janneke@gnu.org>
2016-06-03 14:30:14 +0200

    .init.el: dzn-setup-devel: setup server and daemon buffers.

--- .init.el
+++ .init.el
@@ -6,7 +6,18 @@
     (c-set-offset 'member-init-intro 0)
     (c-set-offset 'innamespace [0])))
 
-(push '("\\.dzn$" . c++-mode) auto-mode-alist)
-
-
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(defun dzn-setup-devel ()
+  (interactive)
+  (save-excursion
+    (shell (get-buffer-create "*daemon*"))
+    (insert "cd ~/development.git/daemon && make debug")
+    (comint-send-input))
+  (save-excursion
+    (shell (get-buffer-create "*server*"))
+    (insert "cd ~/development.git/server && make debug")
+    (comint-send-input))
+  (shell)
+  (insert "cd ~/development.git && make hello")
+  (comint-send-input))
