c91808e0be91023fd6d181918904f5b2ac72decb
Jan Nieuwenhuizen <janneke@gnu.org>
2016-06-09 14:32:54 +0200

    test: aspects: verify: fail upon non-zero exit.

--- test/lib/aspects.js
+++ test/lib/aspects.js
@@ -418,7 +418,7 @@ var aspects = {
       })
       .fail (function(err) {
         console.log ('verify: no baseline=' + baseline);
-        return 'out="$(' + dzn(parameters.session) + ' verify --all -m '+parameters.model+' '+parameters.filename+')"; [ "$out" = "" ] || { echo "$out"; false; }';
+        return 'out="$(' + dzn(parameters.session) + ' verify --all -m '+parameters.model+' '+parameters.filename+')" && [ "$out" = "" ] || { echo "$out"; false; }';
       })
       .then (function(cmd) {
         return util.spawn_sync_shell(cmd);
