3c86e1b2fd0d18b62e4c1356510996da94c9f793
Jan Nieuwenhuizen <janneke@gnu.org>
2016-09-05 12:11:34 +0200

    gaiag: tiny C: DZN_ILLEGAL.

--- gaiag/runtime/c/dzn/config.h
+++ gaiag/runtime/c/dzn/config.h
@@ -31,6 +31,8 @@
 
 #if !DZN_TINY
 
+#define DZN_ILLEGAL(self) self->dzn_info.locator->illegal()
+
 #ifndef DZN_TRACING
 #define DZN_TRACING 1
 #endif
@@ -43,7 +45,11 @@
 #define DZN_LOCATOR_SERVICES 1
 #endif
 
-#endif // !DZN_TINY
+#else // DZN_TINY
+
+#define DZN_ILLEGAL(self) while (0)
+
+#endif // DZN_TINY
 
 #if !DZN_DYNAMIC_QUEUES
 #define DZN_MAX_ARGS_SIZE 24
