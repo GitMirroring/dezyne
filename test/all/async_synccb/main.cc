28685dbbd201c584292db9807cbf40dc3cbf89a5
Jan Nieuwenhuizen <janneke@gnu.org>
2016-12-01 14:10:40 +0100

    test: c++: async_synccb: add handwritten main, enable test.

---
+++ test/all/async_synccb/main.cc
@@ -0,0 +1,82 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2016 Jan Nieuwenhuizen <janneke@gnu.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#include "async_synccb.hh"
+
+#include <dzn/locator.hh>
+#include <dzn/runtime.hh>
+#include <dzn/pump.hh>
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+std::string
+read ()
+{
+  std::string str;
+  {
+    std::string line;
+    while(std::cin >> line) str += (str.empty () ? "" : "\n") + line;
+  }
+  return str;
+}
+
+int main()
+{
+  std::string trace = read ();
+
+  dzn::locator loc;
+  dzn::runtime rt;
+  loc.set(rt);
+  dzn::pump pump;
+  loc.set(pump);
+
+  async_synccb sut(loc);
+
+  sut.dzn_meta.name = "sut";
+  sut.p.meta.requires.port = "p";
+  int t = 0;
+  sut.p.out.cb1 = [t] {std::clog << "sut.p.cb1 -> <external>.p.cb1 [" <<  t << "]" << std::endl;};
+  sut.p.out.cb2 = [t] {std::clog << "sut.p.cb2 -> <external>.p.cb2 [" <<  t << "]" << std::endl;};
+
+  if (0);
+  else if (trace == "p.e\np.cb1\np.return\np.c\np.return")
+    {
+      dzn::blocking (pump, [&] {sut.p.in.e ();sut.p.in.c ();});
+    }
+  else if (trace == "p.e\np.cb1\np.return\np.cb2")
+    {
+      dzn::blocking (pump, [&] {sut.p.in.e ();});
+    }
+  else if (trace == "p.c\np.return")
+    {
+      dzn::blocking (pump, [&] {sut.p.in.c ();});
+    }
+  else
+    {
+      std::clog << "error: invalid trace: " << trace << std::endl;
+      return 1;
+    }
+  return 0;
+}
