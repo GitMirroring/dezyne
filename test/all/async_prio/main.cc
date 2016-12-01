405926af7d1f9601b24ad92267c59fdb49d2d7fa
Jan Nieuwenhuizen <janneke@gnu.org>
2016-12-01 09:05:31 +0100

    test: c++: async_prio: add handwritten main, enable test.

---
+++ test/all/async_prio/main.cc
@@ -0,0 +1,93 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2016 Jan Nieuwenhuizen <janneke@gnu.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#include "async_prio.hh"
+
+#include <dzn/locator.hh>
+#include <dzn/runtime.hh>
+#include <dzn/pump.hh>
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+std::string
+read ()
+{
+  std::string str;
+  {
+    std::string line;
+    while(std::cin >> line) str += (str.empty () ? "" : "\n") + line;
+  }
+  return str;
+}
+
+int main()
+{
+  std::string trace = read ();
+
+  dzn::locator loc;
+  dzn::runtime rt;
+  loc.set(rt);
+  dzn::pump pump;
+  loc.set(pump);
+
+  async_prio sut(loc);
+
+  sut.dzn_meta.name = "sut";
+  sut.p.meta.requires.port = "p";
+  sut.r.meta.provides.port = "r";
+  int t = 0;
+  sut.p.out.cb = [t] {std::clog << "sut.p.cb -> <external>.p.cb [" <<  t << "]" << std::endl;};
+  sut.p.out.ping = [t] {std::clog << "sut.p.ping -> <external>.p.ping [" <<  t << "]" << std::endl;};
+
+  if (0);
+  else if (trace == "p.c\np.return")
+    {
+      dzn::blocking (pump, [&] {sut.p.in.c ();});
+      std::this_thread::sleep_for (std::chrono::milliseconds (1));
+    }
+  else if (trace == "p.e\np.return\np.c\np.return")
+    {
+      dzn::blocking (pump, [&] {sut.p.in.e ();sut.p.in.c ();});
+      std::this_thread::sleep_for (std::chrono::milliseconds (1));
+    }
+  else if (trace == "p.e\np.return\np.cb")
+    {
+      dzn::blocking (pump, [&] {sut.p.in.e ();});
+      std::this_thread::sleep_for (std::chrono::milliseconds (1));
+    }
+  else if (trace == "r.ping\np.ping")
+    {
+      dzn::blocking (pump, [&] {sut.r.out.ping ();});
+      std::this_thread::sleep_for (std::chrono::milliseconds (1));
+    }
+  else
+    {
+      std::clog << "error: invalid trace: " << trace << std::endl;
+      return 1;
+    }
+  return 0;
+}
