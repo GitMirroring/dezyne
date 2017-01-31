61cf8a120e8ccbdfc5ae59c6a987c797d2351b2e
Jan Nieuwenhuizen <janneke@gnu.org>
2017-01-31 09:25:10 +0100

    gaiag: Add geiser-guile setup.
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+((nil
+  .
+  ())
+ (scheme-mode
+  .
+  ((eval
+    .
+    (progn
+      (defun prefix-dir-locals-dir (elt)
+        (concat (locate-dominating-file buffer-file-name ".dir-locals.el") elt))
+      (mapcar
+       (lambda (dir) (add-to-list 'geiser-guile-load-path dir))
+       (mapcar
+        #'prefix-dir-locals-dir
+        '("build/ccache/json"
+          "build/ccache"
+          "gaiag/json"
+          "gaiag"))))))))
