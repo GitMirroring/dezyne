d1e1070cbfa1891152557c0e5b4c19216b5d6c3f
Jan Nieuwenhuizen <janneke@gnu.org>
2017-06-21 21:52:42 +0200

    gdzn: add parse.

---
+++ gaiag/gaiag/commands/parse.scm
@@ -0,0 +1,73 @@
+;;; Dezyne --- Dezyne command line tools
+;;; Copyright © 2017 Jan Nieuwenhuizen <janneke@gnu.org>
+;;;
+;;; This file is part of Dezyne.
+;;;
+;;; Dezyne is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU Affero General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; Dezyne is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+;;; 
+;;; Commentary:
+;;; 
+;;; Code:
+
+;; This file is part of Gaiag, Guile in Asd In Asd in Guile.
+
+(define-module (gaiag commands parse)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
+  #:use-module (ice-9 getopt-long)
+  #:use-module (gaiag misc)
+  #:export (parse-opts
+            main))
+
+(define (parse-opts args)
+  (let* ((option-spec
+          '((help (single-char #\h))
+            (import (single-char #\I) (value #t))
+	    (version (single-char #\V) (value #t))))
+	 (options (getopt-long args option-spec
+		   #:stop-at-first-non-option #t))
+	 (help? (option-ref options 'help #f))
+	 (files (option-ref options '() '())))
+    (or
+     (and help?
+          (stdout "\
+Usage: gdzn parse [OPTION]... [FILE]...
+  -h, --help             display this help and exit
+  -I, --import=DIR+           add DIR to import path
+  -V, --version=VERSION  use service version=VERSION
+")
+          (exit 0)))
+    options))
+
+(define (generator options file-name)
+  (let* ((import-opt (lambda (o) (and (eq? (car o) 'import) (cdr o))))
+         (imports (filter-map import-opt options))
+         (gdzn-debug? (find (cut equal? <> "--debug") (command-line)))
+         (command (string-append
+                   "PATH=" (dirname (car (command-line))) ":bin:../bin:$PATH" ;; FIXME
+                   " generate "
+                   (string-join imports " -I " 'prefix)
+                   " " file-name)))
+    (if gdzn-debug? (stderr "command: ~a\n" command))
+    (display (gulp-pipe command))))
+
+(define (main args)
+  (let* ((options (parse-opts args))
+         (files (option-ref options '() '()))
+         (files (map (lambda (f) (if (not (string-prefix? "/" f)) f (string-drop f 1))) files)))
+    (catch #t
+      (lambda _
+        (generator options (car files)))
+      (lambda _
+        (exit 1)))))
