281c32edf8d92a8a3a626d13952c5aff3871d839
Jan Nieuwenhuizen <janneke@gnu.org>
2017-07-04 12:03:16 +0200

    gdzn run: use --illegal.

--- gaiag/gaiag/commands/run.scm
+++ gaiag/gaiag/commands/run.scm
@@ -68,7 +68,7 @@ Usage: gdzn run [OPTION]... [FILE]...
                    (string-append " -m " model-opt)
                    ;;(string-join imports " -I " 'prefix)
                    " " file-name
-		   "| trace2net.js")))
+		   "| trace2net.js --illegal")))
     (if gdzn-debug? (stderr "command: ~a\n" command))
     (let ((trace (gulp-pipe command)))
       (if strict?
