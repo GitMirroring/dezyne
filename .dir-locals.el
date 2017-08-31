c6701e2a3eba166b45af47df58951a4fa9b79e24
Jan Nieuwenhuizen <janneke@gnu.org>
2017-08-31 21:42:16 +0200

    emacs: .dir-locals.el: include editing settings and dzn-setup-devel.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,47 @@
 ((nil
   .
-  ())
+  ((indent-tabs-mode . nil)
+   (eval
+    .
+    (progn
+      (add-hook 'before-save-hook 'delete-trailing-whitespace nil t)
+      (defun dzn-setup-devel ()
+        (interactive)
+        (let ((top (locate-dominating-file default-directory ".dir-locals.el")))
+          (save-excursion
+            (shell (get-buffer-create "*daemon*"))
+            (insert (format "cd %sdzn-daemon && make debug" top))
+            (comint-send-input))
+          (save-excursion
+            (shell (get-buffer-create "*server*"))
+            (insert (format "cd %sserver && make debug" top))
+            (comint-send-input))
+          (shell)
+          (insert (format "cd %s && make VERBOSE= hello" top))
+          (comint-send-input)))))))
+ ("gaiag/templates"
+  .
+  ((nil
+    .
+    ((eval
+      .
+      (remove-hook 'before-save-hook 'delete-trailing-whitespace t))))))
+ (c++-mode
+  .
+  ((c-basic-offset . 2)
+   (eval
+    .
+    (progn
+      (c-set-offset 'substatement-open 0)
+      (c-set-offset 'member-init-intro 0)
+      (c-set-offset 'innamespace [0])))))
+ (js-mode
+  .
+  ((js-indent-level . 2)))
  (scheme-mode
   .
-  ((eval
+  ((geiser-active-implementations . '(guile))
+   (eval
     .
     (progn
       (defun prefix-dir-locals-dir (elt)
