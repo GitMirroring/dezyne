a5f6936f45190a20d55acd0a6521ac9835c4e730
Henk Katerberg <henk.katerberg@verum.com>
2018-03-13 13:38:49 +0100

    Fix tables

--- gaiag/gaiag/dzn.scm
+++ gaiag/gaiag/dzn.scm
@@ -1,8 +1,10 @@
 ;;; Dezyne --- Dezyne command line tools
 ;;;
 ;;; Copyright © 2014, 2015, 2016, 2017, 2018 Jan Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2018 Henk Katerberg <henk.katerberg@verum.com>
 ;;; Copyright © 2017 Johri van Eerd <johri.van.eerd@verum.com>
 ;;; Copyright © 2017, 2018 Rutger van Beusekom <rutger.van.beusekom@verum.com>
+;;; Copyright © 2018 Henk Katerberg <henk.katerberg@verum.com>
 ;;; Copyright © 2017, 2018 Rob Wieringa <Rob.Wieringa@verum.com>
 ;;;
 ;;; This file is part of Dezyne.
@@ -159,6 +161,9 @@
     (if (member language '(dzn html)) language
         'dzn)))
 
+(define-method (ast->dzn (o <root>))
+  (parameterize ((language 'dzn))
+    (with-output-to-string (dzn:indent (cut x:source o)))))
 (define-method (ast->dzn (o <statement>))
   (parameterize ((language 'dzn))
     (with-output-to-string (dzn:indent (cut x:statement o)))))
