ecef88716a66424a787afd8b2a3af1df7aea6496
Rutger van Beusekom <rutger.van.beusekom@verum.com>
2018-06-08 14:22:07 +0200

    fix thread safe shell includes

--- gaiag/gaiag/parse.scm
+++ gaiag/gaiag/parse.scm
@@ -4,7 +4,6 @@
 ;; Copyright © 2018 Rob Wieringa <Rob.Wieringa@verum.com>
 ;; Copyright © 2014 Paul Hoogendijk <paul.hoogendijk@verum.com>
 ;; Copyright © 2014 Rutger van Beusekom <rutger.van.beusekom@verum.com>
-;; Copyright © 2018 Rob Wieringa <Rob.Wieringa@verum.com>
 ;;
 ;; Gaiag is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU Affero General Public License as
