c92a4109669bb7c38ca6d11ac1da24d80a34fd92
Jan Nieuwenhuizen <janneke@gnu.org>
2019-05-29 21:21:37 +0200

    test: hello_foreign_path; fix test for code.

---
+++ test/all/hello_foreign_path/c++/foreign.hh
@@ -0,0 +1,32 @@
+// Dezyne --- Dezyne command line tools
+// Copyright © 2019 Jan Nieuwenhuizen <janneke@gnu.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#ifndef FOREIGN_HH
+#define FOREIGN_HH
+
+struct foreign: public skel::foreign
+{
+  foreign(const dzn::locator&);
+  void w_world();
+};
+
+#endif // FOREIGN_HH
