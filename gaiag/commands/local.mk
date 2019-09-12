138cbae3fd2d45309a9064c5d767bab7eca82f22
Jan Nieuwenhuizen <janneke@gnu.org>
2019-09-12 13:33:53 +0200

    build: Move scheme Makefile.am snippets into %D%/local.mk.
    * gaiag/parse/local.mk: New file.
    * gaiag/peg/local.mk: New file.
    * gaiag/local.mk: New file.
    * gaiag/commands/local.mk: New file.
    * Makefile.am: Update.

---
+++ gaiag/commands/local.mk
@@ -0,0 +1,41 @@
+# Dezyne --- Dezyne command line tools
+#
+# Copyright © 2019 Jan Nieuwenhuizen <janneke@gnu.org>
+#
+# This file is part of Dezyne.
+#
+# Dezyne is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Dezyne is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Commentary:
+#
+# Code:
+
+dist_%C%_scm_DATA =				\
+ %D%/cat.scm					\
+ %D%/code.scm					\
+ %D%/hello.scm					\
+ %D%/ls.scm					\
+ %D%/lts.scm					\
+ %D%/lts2traces.scm				\
+ %D%/parse.scm					\
+ %D%/traces.scm					\
+ %D%/verify.scm
+
+dist_nocompile_%C%_scm_DATA =
+
+%C%_scmdir = $(guilemoduledir)/%D%
+nocompile_%C%_scmdir = $(%C%_scmdir)
+%C%_godir = $(guileobjectdir)/%D%
+%C%_go_DATA = $(dist_%C%_scm_DATA:%.scm=%.go)
+ALL_GO += $(%C%_go_DATA)
---
+++ gaiag/peg/local.mk
@@ -0,0 +1,37 @@
+# Dezyne --- Dezyne command line tools
+#
+# Copyright © 2019 Jan Nieuwenhuizen <janneke@gnu.org>
+#
+# This file is part of Dezyne.
+#
+# Dezyne is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Dezyne is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Commentary:
+#
+# Code:
+
+dist_%C%_scm_DATA =				\
+ %D%/string-peg.scm				\
+ %D%/simplify-tree.scm				\
+ %D%/codegen.scm				\
+ %D%/cache.scm					\
+ %D%/using-parsers.scm
+
+dist_nocompile_%C%_scm_DATA =
+
+%C%_scmdir = $(guilemoduledir)/%D%
+nocompile_%C%_scmdir = $(%C%_scmdir)
+%C%_godir = $(guileobjectdir)/%D%
+%C%_go_DATA = $(dist_%C%_scm_DATA:%.scm=%.go)
+ALL_GO += $(%C%_go_DATA)
