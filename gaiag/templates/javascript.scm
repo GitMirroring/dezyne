bd1a65aa599848e733b18d247dbeb8ac661493da
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2019-11-28 08:25:04 +0100

    javascript: Support "namespace to module".
    This introduces the "namespace-to-module" concept for javascript: From a
    DZN-file that defines a single namespace, a corresponding javascript module
    is generated.
    
    Also, foreigns now go into their own module.
    
    * gaiag/code/scheme.scm (used-foreigns): Move to ...
    * gaiag/code.scm (code:used-foreigns): ...here.
    (code:module): Remove.
    * gaiag/code/dzn.scm (dzn:namespace): New method.
    * gaiag/code/javascript.scm (javascript:class-name, javascript:module-name,
    javascript:interface-include): New methods.
    * gaiag/templates/javascript.scm (class-name, module-name,
    interface-include): Use them in new templates.
    * gaiag/templates/javascript/component-include@instance,
    gaiag/templates/javascript/interface-include@file-name: Remove.
    * gaiag/templates/include@file-name: Rename to ..
    * gaiag/templates/javascript/require-module@file-name: ...this.
    * gaiag/templates/javascript/async-member-initializer@port,
    gaiag/templates/javascript/injected-instance-initializer@instance,
    gaiag/templates/javascript/main@component,
    gaiag/templates/javascript/model@component,
    gaiag/templates/javascript/model@foreign,
    gaiag/templates/javascript/model@interface,
    gaiag/templates/javascript/model@system,
    gaiag/templates/javascript/non-injected-instance-initializer@instance,
    gaiag/templates/javascript/provided-member-initializer@port,
    gaiag/templates/javascript/required-member-initializer@port,
    gaiag/templates/javascript/source@root: Namespace support.

--- gaiag/templates/javascript.scm
+++ gaiag/templates/javascript.scm
@@ -1,6 +1,7 @@
 ;;; Dezyne --- Dezyne command line tools
 ;;;
 ;;; Copyright © 2018 Rob Wieringa <Rob.Wieringa@verum.com>
+;;; Copyright © 2019 Jan Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of Dezyne.
 ;;;
@@ -22,3 +23,6 @@
 ;;; Code:
 
 (define-templates javascript-namespace-setup javascript:namespace-setup)
+(define-templates class-name javascript:class-name type-infix)
+(define-templates module-name javascript:module-name)
+(define-templates require-module javascript:require-module)
