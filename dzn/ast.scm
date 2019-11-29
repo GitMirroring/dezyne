c87c43f1e78ca596ce78cc01b68822795eccb9e5
Jan Nieuwenhuizen <janneke@gnu.org>
2019-11-29 08:42:53 +0100

    Rename from Gaiag to Dezyne.
    * dzn/ast.scm: Update header, renaming from Gaiag to Dezyne.
    * dzn/c++.scm: Likewise.
    * dzn/c.scm: Likewise.
    * dzn/code.scm: Likewise.
    * dzn/dzn.scm: Likewise.
    * dzn/fifo.scm: Likewise.
    * dzn/goops.scm: Likewise.
    * dzn/indent.scm: Likewise.
    * dzn/javascript.scm: Likewise.
    * dzn/json2scm.scm: Likewise.
    * dzn/misc.scm: Likewise.
    * dzn/parse.scm: Likewise.
    * dzn/scheme.scm: Likewise.
    * dzn/templates/c++.scm: Likewise.
    * dzn/templates/code.scm: Likewise.
    * dzn/templates/dzn.scm: Likewise.
    * dzn/wfc.scm: Likewise.

--- dzn/ast.scm
+++ dzn/ast.scm
@@ -1,23 +1,25 @@
-;; This file is part of Gaiag, Guile in Asd In Asd in Guile.
-;;
-;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019 Jan Nieuwenhuizen <janneke@gnu.org>
-;; Copyright © 2018 Filip Toman <filip.toman@verum.com>
-;; Copyright © 2017, 2018, 2019 Rob Wieringa <Rob.Wieringa@verum.com>
-;; Copyright © 2017, 2018 Johri van Eerd <johri.van.eerd@verum.com>
-;; Copyright © 2014, 2018 Rutger van Beusekom <rutger.van.beusekom@verum.com>
-;;
-;; Gaiag is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU Affero General Public License as
-;; published by the Free Software Foundation, either version 3 of the
-;; License, or (at your option) any later version.
-;;
-;; Gaiag is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU Affero General Public License for more details.
-;;
-;; You should have received a copy of the GNU Affero General Public License
-;; along with Gaiag.  If not, see <http://www.gnu.org/licenses/>.
+;;; Dezyne --- Dezyne command line tools
+;;;
+;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019 Jan Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2014, 2018 Rutger van Beusekom <rutger.van.beusekom@verum.com>
+;;; Copyright © 2017, 2018, 2019 Rob Wieringa <Rob.Wieringa@verum.com>
+;;; Copyright © 2017, 2018 Johri van Eerd <johri.van.eerd@verum.com>
+;;; Copyright © 2018 Filip Toman <filip.toman@verum.com>
+;;;
+;;; This file is part of Dezyne.
+;;;
+;;; Dezyne is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU Affero General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; Dezyne is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
 ;;;
 ;;; Commentary:
 ;;;
