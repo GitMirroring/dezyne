7397e53affc15c8f1bba730f35d2d2b67278a656
Jan Nieuwenhuizen <janneke@gnu.org>
2020-02-06 12:33:42 +0100

    test: Fix hello_recursive and recursive tail call check.
    * dzn/wfc.scm (recurses?): New function.
    (tail-recursion): Use it to fix recursive call in tail position test.
    * dzn/parse/ast.scm (recurses?): Robustify.
    * test/all/hello_recursive/hello_recursive.dzn: Remove valued recursive
    function; that is not (yet) supported.
    * test/all/wf_functionTailRecursion/baseline/verify/wf_functionTailRecursion.stderr:
    Update baseline.

--- dzn/wfc.scm
+++ dzn/wfc.scm
@@ -533,20 +533,44 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; helper functions
 
+(define* (recurses? call #:optional (seen '()))
+  (define (call-statement ast)
+    (match ast
+      (($ <call>) ast)
+      ((and ($ <assign>) (? (compose (is? <call>) .expression)) (= .expression call)) call)
+      ((and ($ <variable>) (? (compose (is? <call>) .expression)) (= .expression call)) call)
+      (_ #f)))
+  (define (.function-name call)
+    (or (and=> (as (.function call) <function>) .name) (.function call)))
+  (or (and (member (.function.name call) seen) seen)
+      (let ((function (.function call)))
+        (and function
+             (let* ((compound (.statement function))
+                    (call-statements (tree-collect call-statement compound))
+                    (calls (delete-duplicates (map call-statement call-statements)
+                                              (lambda (a b) (string< (or (.function.name a) "")
+                                                                     (or (.function.name b) ""))))))
+               (any identity
+                    (map (lambda (c)
+                           (recurses? c (cons (.function.name call) seen)))
+                         calls)))))))
+
 (define-method (tail-recursion (o <call>))
-  (let ((function (parent o <function>)))
+  (let ((function (parent o <function>))
+        (called-function (.function o)))
     (if (or (not function)
-            (not (.recursive function))) '()
+            (not (and=> (recurses? o) (cut member (.name function) <>)))) '()
             (let* ((continuation ((compose car wfc:continuation) o))
                    (continuation (if (or (parent o <assign>) (parent o <variable>)) ((compose car (@@ (dzn makreel) makreel:continuation)) continuation) continuation))
                    (continuation (and continuation
                                       (not (ast:eq? continuation (.statement (parent o <function>))))
-                                      (not (is-a? continuation <return>))
                                       continuation)))
-              (if continuation `(,(wfc-error o "recursive function not in tail call")
-                                 ,(wfc-error continuation "next statement"))
-                  '())))))
-
+              (cond ((is-a? continuation <return>)
+                     `(,(wfc-error o "valued recursive functions not supported yet")
+                       ,(wfc-error continuation "statement after call")))
+                    (continuation `(,(wfc-error o "no statement allowed after recursive function call")
+                                    ,(wfc-error continuation "statement after call")))
+                    (else '()))))))
 
 (define-method (mixing-declarative-imperative (o <compound>))
   (if (ast:declarative? o)
