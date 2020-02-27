487998db099750b7d784257904fba313331b4a42
Jan Nieuwenhuizen <janneke@gnu.org>
2020-02-27 09:35:52 +0100

    build: Relicense dzn-runtime under lesser gpl3+.
    * COPYING.LESSER: New file.
    * README: Mention dzn-runtime and its license.
    * runtime/: Relicense dzn-runtime under the GNU Lesser General Public
    Licence 3.0 or later.

--- runtime/c++/dzn/sexp.hh
+++ runtime/c++/dzn/sexp.hh
@@ -1,21 +1,21 @@
-// Dezyne --- Dezyne command line tools
+// dzn-runtime -- Dezyne runtime library
 //
 // Copyright © 2019 Jan Nieuwenhuizen <janneke@gnu.org>
 //
-// This file is part of Dezyne.
+// This file is part of dzn-runtime.
 //
-// Dezyne is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Affero General Public License as
+// dzn-runtime is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// Dezyne is distributed in the hope that it will be useful, but
+// dzn-runtime is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Affero General Public License for more details.
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Affero General Public
-// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public
+// License along with dzn-runtime.  If not, see <http://www.gnu.org/licenses/>.
 //
 // Commentary:
 //
