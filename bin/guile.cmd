ad0ea6172299887a142b6aa71b5e09729b419420
Jan Nieuwenhuizen <janneke@gnu.org>
2020-03-16 13:52:07 +0100

    pack: Cleanup mingw distribution.
    * Makefile.am (GUIX_PACKAGE_PATH): Simplify.  Move executables into bin.
    * bin/guile.cmd: Support that.

--- bin/guile.cmd
+++ bin/guile.cmd
@@ -18,5 +18,5 @@ set GUILE_AUTO_COMPILE=0
 set GUILE_LOAD_PATH=%GUILE_LOAD_PATH%;%prefix%/share/guile/site/2.2;%prefix%/share/guile/2.2
 set GUILE_LOAD_COMPILED_PATH=%GUILE_LOAD_COMPILED_PATH%;%prefix%/lib/guile/2.2/site-ccache;%prefix%/lib/guile/2.2/ccache
 set HOME=%USERPROFILE%
-set PATH=%~p0;%~p0/lib;%PATH%
+set PATH=%~p0;%~p0/bin;%~p0/lib;%PATH%
 guile.exe %*
