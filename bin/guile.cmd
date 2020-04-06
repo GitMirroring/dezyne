64ef2cad72f80e836bdd75a90ffcc8b51730fc87
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-04-06 22:29:08 +0200

    pack: Update `guile.cmd' from ide.
    * bin/guile.cmd: Update from dezyne-ide.

--- bin/guile.cmd
+++ bin/guile.cmd
@@ -3,18 +3,9 @@ setlocal
 if not exist tmp mkdir tmp
 set LANG=C
 set LC_ALL=C
-set LC_ADDRESS=C
-set LC_IDENTIFICATION=C
-set LC_MEASUREMENT=C
-set LC_MONETARY=C
-set LC_NAME=C
-set LC_NUMERIC=C
-set LC_PAPER=C
-set LC_TELEPHONE=C
-set LC_TIME=C
+set LC_CTYPE=C
 set TMPDIR=tmp
 set dir=.
-set DZN_UNINSTALLED=1
 set absdir=%~p0
 set PREFIX=%absdir:\=/%
 set GUILE_AUTO_COMPILE=0
