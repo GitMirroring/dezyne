0fa7315509165fccaa85ca2a643907feba64bad6
Paul Hoogendijk <paul.hoogendijk@verum.com>
2020-04-20 23:22:26 +0200

    verify: Only add one element of acceptance-set to trace.
    Concatenating all alternatives from the set creates an impossible trace;
    use just one alternative.
    
    * dzn/verify/pipeline.scm (do-refinement): Split acceptance set into
    sorted list of events.
    (check-compliance): Use the first element of acceptance set to lengthen
    the trace.

--- test/all/local.mk
+++ test/all/local.mk
@@ -110,6 +110,7 @@ REGRESSION_TESTS =				\
  %D%/compliance_out_inevitable_illegal		\
  %D%/compliance_out_nondet			\
  %D%/compliance_out_sync			\
+ %D%/compliance_out_two 			\
  %D%/compliance_provides_bool			\
  %D%/compliance_provides_illegal5		\
  %D%/compliance_provides_int			\
