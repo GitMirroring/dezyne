fcc2ebbe6e9af6f0008418371807a25a374fdbfc
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-10-16 11:56:02 +0200

    test: hello_imported: Resurrect and enable.
    * test/all/hello_imported/hello_imported.dzn: Move '#file ' marker to
    top; do not lie about file name.
    * test/parser/hello_imported: Add symlink.
    (hello_import): Move to ..
    (hello_imported): ...this.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -148,6 +148,7 @@ REGRESSION_TESTS =				\
  %D%/hello_ifelse				\
  %D%/hello_ifif					\
  %D%/hello_implicit_illegal			\
+ %D%/hello_imported				\
  %D%/hello_import_component			\
  %D%/hello_inevitable				\
  %D%/hello_inevitable_action			\
