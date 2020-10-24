e0018bc552db07f2be2f0eb96cc7ed76ccd5c810
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-10-24 15:37:18 +0200

    dzn: Add -c,--core option; support running ide commands.
    * dzn/gdzn.scm (parse-opts): Add -c,--core option.  Also show ide
    commands unless --core.
    (run-command): Add #:core? parameter.  Prefer ide commands unless core?
    (main): Pass core? parameter.

--- dzn/gdzn.scm
+++ dzn/gdzn.scm
@@ -36,33 +36,46 @@
 
 (define (parse-opts args)
   (let* ((option-spec
-	  '((debug (single-char #\d))
+	  '((core (single-char #\c))
+            (debug (single-char #\d))
             (help (single-char #\h))
             (json (single-char #\j))
 	    (peg (single-char #\p))
 	    (verbose (single-char #\v))
 	    (version (single-char #\V))))
 	 (options (getopt-long args option-spec
-		   #:stop-at-first-non-option #t))
+		               #:stop-at-first-non-option #t))
+	 (core? (option-ref options 'core #f))
 	 (help? (option-ref options 'help #f))
 	 (files (option-ref options '() '()))
 	 (usage? (and (not help?) (null? files)))
 	 (version? (option-ref options 'version #f)))
+
+    (define (list-commands dir)
+      (map (cut basename <> ".go")
+           (filter (disjoin
+                    (cute string-contains <> dir)
+                    (conjoin (negate (const core?))
+                             (cute string-contains <> "/ide/commands/")))
+                   (append-map (cute find-files <> "\\.go$")
+                               (filter directory-exists?
+                                       %load-compiled-path)))))
 
     (or
      (and version?
 	  ((stdout "dzn ~a\n" %version) (exit 0)))
      (and (or help? usage?)
           ((or (and usage? stderr) stdout)
-           (let ((commands (delete-duplicates
-                            (map (cut basename <> ".go")
-                                 (filter (cut string-contains <> "/dzn/commands/")
-                                         (append-map (cut find-files <> "\\.go$")
-                                                     (filter directory-exists?
-                                                             %load-compiled-path))))
-                            string=?)))
+           (let* ((core-commands (list-commands "/dzn/commands/"))
+                  (ide-commands (list-commands "/ide/commands/"))
+                  (commands (sort
+                             (delete-duplicates
+                              (append core-commands ide-commands)
+                              string=?)
+                             string<)))
              (string-append "\
 Usage: dzn [OPTION]... COMMAND [COMMAND-ARGUMENT...]
+  -c, --core             run core commands only
   -d, --debug            enable debug ouput
   -h, --help             display this help
   -j, --json             output json
@@ -71,20 +84,22 @@ Usage: dzn [OPTION]... COMMAND [COMMAND-ARGUMENT...]
   -V, --version          display version
 
 Commands:"
-                                  (string-join commands "\n  " 'prefix)
-                                  "
+                            (string-join commands "\n  " 'prefix)
+                            "
 
 Use \"dzn COMMAND --help\" for command-specific information.
 ")))
-	   (exit (or (and usage? 2) 0)))
+	  (exit (or (and usage? 2) 0)))
      options)))
 
 (define parse-opts (pure-funcq parse-opts))
 
-(define (run-command args)
+(define* (run-command args #:key core?)
   (let* ((command (string->symbol (car args)))
-         (module (resolve-module `(dzn commands ,command)))
-         (main (and module (false-if-exception (module-ref module 'main)))))
+         (core-module (resolve-module `(dzn commands ,command)))
+         (ide-module (resolve-module `(ide commands ,command)))
+         (main (or (and (not core?) (false-if-exception (module-ref ide-module 'main)))
+                   (false-if-exception (module-ref core-module 'main)))))
     (unless main
       (format (current-error-port) "dzn: no such command: ~a\n" command)
       (exit 1))
@@ -94,6 +109,8 @@ Use \"dzn COMMAND --help\" for command-specific information.
   (let* ((options (parse-opts args))
          (command-args (option-ref options '() '()))
          (command (and (pair? command-args) (car command-args)))
-         (debug? (option-ref options 'debug #f)))
-    (if (getenv "DZN_REPL") (call-with-error-handling (cut run-command command-args))
-        (run-command command-args))))
+         (debug? (option-ref options 'debug #f))
+         (core? (option-ref options 'core #f)))
+    (if (getenv "DZN_REPL") (call-with-error-handling
+                             (lambda _ (run-command command-args #:core? core?)))
+        (run-command command-args #:core? core?))))
