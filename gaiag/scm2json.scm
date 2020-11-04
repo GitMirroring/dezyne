e5add272ccbf67eb84786db3999670dd59280bdc
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-11-04 08:54:40 +0100

    Use exit EXIT_FAILURE and EXIT_OTHER_FAILURE.
    Use EXIT_FAILURE, EXIT_SUCCESS, and EXIT_OTHER_FAILURE for "could not
    perform operation" throughout.
    
    See <https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html>
    
    * gaiag/command-line.scm (EXIT_OTHER_FAILURE): New variable.

--- gaiag/scm2json.scm
+++ gaiag/scm2json.scm
@@ -19,6 +19,7 @@
   #:use-module (system repl error-handling)
   #:use-module (ice-9 getopt-long)
   #:use-module (json)
+  #:use-module (gaiag command-line)
   #:use-module (gaiag misc)
   #:export (main))
 
@@ -49,7 +50,7 @@ Examples:
   echo \"(console.arm console.disarm sensor.disabled)\" | ./scm2json
   ./gaiag -l simulate -t \"$(cat examples/Alarm-trail.scm)\" examples/Alarm.dzn | ./scm2json
 ")
-	   (exit (or (and usage? 2) 0)))
+	   (exit (or (and usage? EXIT_OTHER_FAILURE) 0)))
      options)))
 
 (define (->json files)
