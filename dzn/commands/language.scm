b321ecd2c80a504ddf815737ab5ef82068ffd2f1
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-11-27 07:12:38 +0100

    commands: Allow options after file names.
    Revert long standing thinko, we need to stop parsing to find the command
    in the top level script; the command scripts can be more relaxed.
    
    * dzn/commands/cat.scm (parse-opts): Remove #:stop-at-first-non-option.
    * dzn/commands/code.scm (parse-opts): Likewise.
    * dzn/commands/hello.scm (parse-opts): Likewise.
    * dzn/commands/language.scm (parse-opts): Likewise.
    * dzn/commands/ls.scm (parse-opts): Likewise.
    * dzn/commands/parse.scm (parse-opts): Likewise.
    * dzn/commands/trace.scm (parse-opts): Likewise.
    * dzn/commands/traces.scm (parse-opts): Likewise.
    * dzn/commands/verify.scm (parse-opts): Likewise.
    * dzn/scm2json.scm (parse-opts): Likewise.

--- dzn/commands/language.scm
+++ dzn/commands/language.scm
@@ -60,8 +60,7 @@
             (offset (value #t))
             (point (single-char #\p) (value #t))
             (verbose (single-char #\v))))
-         (options (getopt-long args option-spec
-		               #:stop-at-first-non-option #t))
+         (options (getopt-long args option-spec))
          (help? (option-ref options 'help #f))
 	 (files (option-ref options '() '()))
          (usage? (and (not help?) (null? files))))
