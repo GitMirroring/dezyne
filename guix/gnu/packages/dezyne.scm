2f312472762b513af8ced28b69ff449b38c9725c
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-12-02 15:42:05 +0100

    guix: dezyne: Update to 2.11.0.rc6.
    * guix/gnu/packages/dezyne.scm (dezyne)[source]: : Update to 2.11.0.rc6.

--- guix/gnu/packages/dezyne.scm
+++ guix/gnu/packages/dezyne.scm
@@ -54,14 +54,14 @@
 (define-public dezyne
   (package
     (name "dezyne")
-    (version #!dezyne!# "2.11.0.rc5")
+    (version #!dezyne!# "2.11.0.rc6")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://dezyne.org/download/dezyne/"
                            name "-" version ".tar.gz"))
        (sha256
-        (base32 #!dezyne!# "1vznf98n4wmr0h14x37dx9yw2ya8xmqw1h9xxhqb77nqdkap6p39"))))
+        (base32 #!dezyne!# "1i29j5yg5llg1jqx002cyffxrx5nk2k8q6snj265qq9zvmc4yd1a"))))
     (inputs `(("bash" ,bash-minimal)
               ("guile" ,guile-2.2)
               ("m4-cw" ,m4-changeword)
