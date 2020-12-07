9f941622cf6945c8aa3f356b37c1181cc24c4733
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-12-07 10:38:27 +0100

    Revert "dzn: Add -c,--core option; support running ide commands."
    Reverted after discussion with logocp on IRC.
    
    This reverts commit 030dd42a177d4453f28f52a7bbb6d0cb4daa455e.

--- dzn/script.scm
+++ dzn/script.scm
@@ -37,16 +37,14 @@
 
 (define (parse-opts args)
   (let* ((option-spec
-	  '((core (single-char #\c))
-            (debug (single-char #\d))
+	  '((debug (single-char #\d))
             (help (single-char #\h))
             (json (single-char #\j))
 	    (peg (single-char #\p))
 	    (verbose (single-char #\v))
 	    (version (single-char #\V))))
 	 (options (getopt-long args option-spec
-		               #:stop-at-first-non-option #t))
-	 (core? (option-ref options 'core #f))
+		   #:stop-at-first-non-option #t))
 	 (help? (option-ref options 'help #f))
 	 (files (option-ref options '() '()))
 	 (usage? (and (not help?) (null? files)))
@@ -54,10 +52,7 @@
 
     (define (list-commands dir)
       (map (cut basename <> ".go")
-           (filter (disjoin
-                    (cute string-contains <> dir)
-                    (conjoin (negate (const core?))
-                             (cute string-contains <> "/ide/commands/")))
+           (filter (cute string-contains <> dir)
                    (append-map (cute find-files <> "\\.go$")
                                (filter directory-exists?
                                        %load-compiled-path)))))
@@ -67,16 +62,10 @@
       (exit EXIT_SUCCESS))
     (when (or help? usage?)
       (let* ((port (if usage? (current-error-port) (current-output-port)))
-             (core-commands (list-commands "/dzn/commands/"))
-             (ide-commands (list-commands "/ide/commands/"))
-             (commands (sort
-                        (delete-duplicates
-                         (append core-commands ide-commands)
-                         string=?)
-                        string<)))
+             (commands (list-commands "/dzn/commands/"))
+             (commands (sort (delete-duplicates commands string=?) string<)))
         (format port "\
 Usage: dzn [OPTION]... COMMAND [COMMAND-ARGUMENT...]
-  -c, --core             run core commands only
   -d, --debug            enable debug ouput
   -h, --help             display this help
   -j, --json             output json
@@ -89,17 +78,15 @@ Commands:~a
 Use \"dzn COMMAND --help\" for command-specific information.
 "
                 (string-join commands "\n  " 'prefix)))
-      (exit (or (and usage? EXIT_OTHER_FAILURE) 0)))
+      (exit (or (and usage? EXIT_OTHER_FAILURE) EXIT_SUCCESS)))
     options))
 
 (define parse-opts (pure-funcq parse-opts))
 
-(define* (run-command args #:key core?)
+(define (run-command args)
   (let* ((command (string->symbol (car args)))
-         (core-module (resolve-module `(dzn commands ,command)))
-         (ide-module (resolve-module `(ide commands ,command)))
-         (main (or (and (not core?) (false-if-exception (module-ref ide-module 'main)))
-                   (false-if-exception (module-ref core-module 'main)))))
+         (module (resolve-module `(dzn commands ,command)))
+         (main (and module (false-if-exception (module-ref module 'main)))))
     (unless main
       (format (current-error-port) "dzn: no such command: ~a\n" command)
       (exit EXIT_OTHER_FAILURE))
@@ -109,8 +96,6 @@ Use \"dzn COMMAND --help\" for command-specific information.
   (let* ((options (parse-opts args))
          (command-args (option-ref options '() '()))
          (command (and (pair? command-args) (car command-args)))
-         (debug? (option-ref options 'debug #f))
-         (core? (option-ref options 'core #f)))
-    (if (getenv "DZN_REPL") (call-with-error-handling
-                             (lambda _ (run-command command-args #:core? core?)))
-        (run-command command-args #:core? core?))))
+         (debug? (option-ref options 'debug #f)))
+    (if (getenv "DZN_REPL") (call-with-error-handling (cut run-command command-args))
+        (run-command command-args))))
