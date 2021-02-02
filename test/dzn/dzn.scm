0484749194325f26d4b4f7c004242bb51e8b2836
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-02-02 09:53:16 +0100

    test: Enable deadlock tests in run-lts, run-simulate.
    * test/dzn/dzn.scm (run-lts, run-simulate): Do not skip deadlock.

--- test/dzn/dzn.scm
+++ test/dzn/dzn.scm
@@ -472,8 +472,7 @@ output, and standard error as three values."
 (define (run-simulate file-name)
   (format #t "** stage: simulate\n")
   (let ((traces (find-files file-name ".*trace.*$")))
-    (or (and (string-contains file-name "deadlock") #t)
-        (and (string-contains file-name "external") #t)
+    (or (and (string-contains file-name "external") #t)
         (and (string-contains file-name "livelock") #t)
         (and (string-contains file-name "queuefull") #t)
         (and-map (cut run-simulate-trace file-name <>) traces))))
@@ -498,7 +497,6 @@ output, and standard error as three values."
         (skip? file-name
                "traces"
                "lts")
-        (and (string-contains file-name "deadlock") #t)
         (and (string-contains file-name "external") #t)
         (and (string-contains file-name "livelock") #t)
         (and (string-contains file-name "queuefull") #t)
