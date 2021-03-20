92fb828dc6ce66550f689173fd1c40bdfe3d1620
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-03-20 09:11:10 +0100

    vm: Remove global for livelock check.
    * dzn/vm/run.scm (livelock-limit): Rename to...
    (%livelock-trace-threshold): ...this, and change to a parameter.
    (livelock?): Update accordingly.
    
    Co-authored-by: Rutger van Beusekom <rutger.van.beusekom@verum.com>.

--- dzn/vm/run.scm
+++ dzn/vm/run.scm
@@ -139,23 +139,23 @@ in the same component."
          (pc (clone pc #:status error)))
     (cons pc (cdr trace))))
 
-(define livelock-limit 42)
-
-(define (livelock? trace)
-  (define* (livelock-check trace)
-    (let ((trace (filter (lambda (pc) (and (is-a? (.instance pc) <runtime:component>)
-                                           (not (is-a? (.statement pc) <initial-compound>)))) trace)))
-      (and (pair? trace)
-           (member (car trace) (cdr trace) pc:eq?))))
-  (define (suffixes trace)
-    (let loop ((res '()) (trace trace))
-      (if (null? trace) res
-          (loop (cons trace res) (cdr trace)))))
-  (if (< (length trace) livelock-limit) #f
-      (let ((lifelock-trace (find livelock-check (suffixes trace))))
-        (when (not lifelock-trace)
-          (set! livelock-limit (* 2 livelock-limit)))
-        lifelock-trace)))
+(define %livelock-trace-threshold (make-parameter 42))
+(define livelock?
+  (lambda (trace)
+    (define* (livelock-check trace)
+      (let ((trace (filter (lambda (pc) (and (is-a? (.instance pc) <runtime:component>)
+                                             (not (is-a? (.statement pc) <initial-compound>)))) trace)))
+        (and (pair? trace)
+             (member (car trace) (cdr trace) pc:eq?))))
+    (define (suffixes trace)
+      (let loop ((res '()) (trace trace))
+        (if (null? trace) res
+            (loop (cons trace res) (cdr trace)))))
+    (if (< (length trace) (%livelock-trace-threshold)) #f
+        (let ((lifelock-trace (find livelock-check (suffixes trace))))
+          (when (not lifelock-trace)
+            (%livelock-trace-threshold (* 2 (%livelock-trace-threshold))))
+          lifelock-trace))))
 
 (define-method (extend-trace (trace <list>))
   "Return a list of traces, produced by appending TRACE to each of the
