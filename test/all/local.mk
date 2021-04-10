b12990453e20254bd5629b61467881cdf4c7268b
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-04-10 16:32:35 +0200

    test: undefined/component/argument: Enable; update baseline.
    The current output seems much less nice than the baseline advertised;
    this was the consequence of
    
        abf6b714b35b00149af368670c883fa4f6d6d3dd
        peg: Fix deep backtracking on component-state.dzn.
    
    * test/all/local.mk (PARSER_TESTS): Add undefined/component/argument.
    * test/all/undefined/component/argument/baseline/verify/argument.stderr:
    Update baseline.

--- test/all/local.mk
+++ test/all/local.mk
@@ -378,6 +378,7 @@ PARSER_TESTS =					\
  %D%/wf_undefined				\
  %D%/wf_variableInitExpression			\
  %D%/undefined/component/action			\
+ %D%/undefined/component/argument		\
  %D%/undefined/component/guard			\
  %D%/undefined/component/if			\
  %D%/undefined/component/port			\
