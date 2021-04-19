50e772e07afaef91aa20be0fe881c10c71857818
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-04-19 09:59:45 +0200

    test: hello_namespace_foreign: Update for c.
    * test/all/hello_namespace_foreign/c/library_foreign.c,
    test/all/hello_namespace_foreign/c/library_foreign.h: New files.

---
+++ test/all/hello_namespace_foreign/c/library_foreign.h
@@ -0,0 +1,38 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2018 Filip Toman <filip.toman@verum.com>
+// Copyright © 2021 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#ifndef LIBRARY_FOREIGN_H
+#define LIBRARY_FOREIGN_H
+
+typedef struct library_foreign_t library_foreign;
+typedef struct library_foreign_skel_t library_foreign_skel;
+
+struct library_foreign_t {
+  library_foreign_skel base;
+  /* space for variables */
+
+  /* end of space */
+};
+
+#endif /* LIBRARY_FOREIGN_H */
