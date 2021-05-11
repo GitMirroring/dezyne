57e61383fb5888dbe84e934bab3b3cec8e4de320
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-05-11 14:56:42 +0200

    DRAFT parse: Implement shared state.
    * dzn/goops.scm (<shared-var>): New class.
    * dzn/parse/peg.scm (peg:parse): Parse it.
    * dzn/parse/ast.scm (parse-tree->ast): Cater for it.
    * dzn/ast.scm (ast:equal?<shared-var>, .port<shared-var>,
    .variable<shared-var>): New specializations.

--- dzn/parse/peg.scm
+++ dzn/parse/peg.scm
@@ -254,11 +254,12 @@ or-expression <- and-expression OR or-expression# / and-expression
 and-expression <- compare-expression AND and-expression# / compare-expression
 compare-expression <- plus-min-expression !LEFT-ARROW COMPARE plus-min-expression# / plus-min-expression
 plus-min-expression <- not-expression (PLUS / MINUS) plus-min-expression# / not-expression
-not-expression <- minus / not / group / dollars / (!var !is-port enum-literal / literal / call / var !DOT / action / interface-action / field-test / unknown-identifier)
+not-expression <- minus / not / group / dollars / (shared-var / !var !is-port enum-literal / literal / call / var !DOT / action / interface-action / field-test / unknown-identifier)
 not <-- NOT not-expression#
 minus <-- UMINUS not-expression#
 enum-literal <-- global? scope name
 field-test <-- !is-port var DOT# name#
+shared-var <-- is-port COLON name#
 literal <-- NUMBER / FALSE / TRUE
 group <-- PAREN-OPEN expression PAREN-CLOSE#
 
