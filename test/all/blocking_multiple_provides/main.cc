ff07f36d6d42c25cbaf84c8bdf55a19997299aa6
Rutger van Beusekom <rutger@dezyne.org>
2021-08-11 15:35:19 +0200

    test: Add blocking_multiple_provides.
    * test/all/blocking_multiple_provides/META,
    test/all/blocking_multiple_provides/blocking_multiple_provides.dzn,
    test/all/blocking_multiple_provides/trace: New files.
    * test/all/local.mk (REGRESSION_TESTS): Add test.

---
+++ test/all/blocking_multiple_provides/main.cc
@@ -0,0 +1,62 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2021 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+// Copyright © 2021 Paul Hoogendijk <paul@dezyne.org>
+// Copyright © 2021 Rutger van Beusekom <rutger@dezyne.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#include "blocking_multiple_provides.hh"
+
+#include <thread>
+
+#include <dzn/locator.hh>
+#include <dzn/runtime.hh>
+#include <dzn/pump.hh>
+
+int
+main ()
+{
+  dzn::locator loc;
+  dzn::runtime rt;
+  loc.set (rt);
+  blocking_multiple_provides sut (loc);
+  sut.dzn_meta.name = "sut";
+  sut.r.meta.require.name = "r";
+
+  sut.r.in.hello = [&]
+  {
+    std::clog << "sut.bmp.r.hello -> <external>.r.hello\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    sut.r.out.world();
+    std::clog << "sut.bmp.r.return -> <external>.r.return\n";
+  };
+
+  std::thread t([&]{
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    sut.right.in.hello ();
+  });
+
+  sut.left.in.hello ();
+
+  t.join();
+
+  return 0;
+}
