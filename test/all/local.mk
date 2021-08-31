f0768a80147367c755088eb49e40ae296de126d1
Rutger van Beusekom <rutger.van.beusekom@verum.com>
2021-08-31 17:16:49 +0200

    test: Add compliance_external_inevitable.
    Reproduce external compliance not reported by dzn simulate as reported
    by Joran Jessurun <joran.jessurun@ajmail.com>.
    
    * test/all/compliance_external_inevitable/baseline/simulate/compliance_external_inevitable,
    test/all/compliance_external_inevitable/baseline/simulate/compliance_external_inevitable.stderr,
    test/all/compliance_external_inevitable/baseline/verify/compliance_external_inevitable,
    test/all/compliance_external_inevitable/baseline/verify/compliance_external_inevitable.stderr,
    test/all/compliance_external_inevitable/compliance_external_inevitable.dzn,
    test/all/compliance_external_inevitable/trace: New files.
    * test/all/local.mk (REGRESSION_TESTS): Add test.

--- test/all/local.mk
+++ test/all/local.mk
@@ -151,6 +151,7 @@ REGRESSION_TESTS =				\
  %D%/compliance_nonsynchronous_sync		\
  %D%/compliance_implicit_illegal		\
  %D%/compliance_livelock			\
+ %D%/compliance_external_inevitable		\
  %D%/compliance_failures_choice			\
  %D%/compliance_failures_illegal		\
  %D%/compliance_failures_inevitable		\
