429deb6df8b917be13ea4a49025c8605e94f61db
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-11-26 10:09:42 +0100

    emacs: Add and update copyright headers.
    Fixes https://savannah.nongnu.org/task/?16067.
    Reported via savannah by Ineiev <ineiev@gnu.org>.
    
    * emacs/pre-inst-env.el: Add copyright header.
    * emacs/dzn-mode.el: Update copyright header.

--- emacs/pre-inst-env.el
+++ emacs/pre-inst-env.el
@@ -1,7 +1,30 @@
+;;; Dezyne --- Dezyne command line tools
+;;;
+;;; Copyright © 2020 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+;;;
+;;; This file is part of Dezyne.
+;;;
+;;; Dezyne is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU Affero General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; Dezyne is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+;;; Commentary:
+;;;
 ;;; To put Emacs in pre-inst-env environment:
 ;;; echo $GUIX_ENVIRONMENT => /gnu/store/...-profile
 ;;; M-x guix-set-emacs-environment RET /gnu/store/...-profile RET
 ;;; M-X ide:pre-inst-env
+;;;
+;;; Code:
 
 (defun add-to-env-path (dir name)
   (let* ((current-value (getenv name))
