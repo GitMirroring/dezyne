33cae97bb7369fe6fe7557e2a227015722a0008d
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-01-07 09:18:23 +0100

    scheme: Support foreign injected trace output.
    * dzn/ast.scm (ast:direction)[port]: New specialization.
    * dzn/templates/scheme/injected-require-initializer@port: Use it in new
    template.
    * dzn/templates/scheme/model@component: Use it.

--- dzn/ast.scm
+++ dzn/ast.scm
@@ -1,6 +1,6 @@
 ;;; Dezyne --- Dezyne command line tools
 ;;;
-;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2014, 2018, 2020, 2021 Rutger van Beusekom <rutger@dezyne.org>
 ;;; Copyright © 2017, 2018, 2019, 2020 Rob Wieringa <rma.wieringa@gmail.com>
 ;;; Copyright © 2017, 2018, 2020 Johri van Eerd <vaneerd.johri@gmail.com>
@@ -335,6 +335,10 @@
 
 (define-method (ast:required+async (o <component-model>))
   (append (ast:requires-port* o) (ast:async-port* o)))
+
+(define-method (ast:direction (o <port>))
+  (if (ast:provides? o) 'in
+      'out))
 
 (define-method (ast:direction (o <trigger>))
   (.direction (.event o)))
