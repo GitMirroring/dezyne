e665ed820563706b4ebf184e9b4446f739035093
Paul Hoogendijk <paul@dezyne.org>
2022-04-04 14:42:38 +0200

    vm: Avoid false livelock due to queuing of events.
    * dzn/vm/util.scm (serialize): Also serialize queue.
    
    * test/all/livelock_synchronous2/baseline/simulate/livelock_synchronous2,
    test/all/queuefull_component0/baseline/simulate/queuefull_component0:
    Update baseline.

--- dzn/vm/util.scm
+++ dzn/vm/util.scm
@@ -3,7 +3,7 @@
 ;;; Copyright © 2019, 2020, 2021, 2022 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2018, 2019 Rob Wieringa <rma.wieringa@gmail.com>
 ;;; Copyright © 2018, 2019, 2021, 2022 Rutger van Beusekom <rutger@dezyne.org>
-;;; Copyright © 2021 Paul Hoogendijk <paul@dezyne.org>
+;;; Copyright © 2021, 2022 Paul Hoogendijk <paul@dezyne.org>
 ;;;
 ;;; This file is part of Dezyne.
 ;;;
@@ -933,6 +933,9 @@ event with COLLATERAL-BLOCK?."
                                (display " " port)
                                (display (cons x (->sexp y)) port)))
                 (.variables o)))
+    (when (pair? (.q o))
+      (display " " port)
+      (display `("*q*" . ,(map trigger->string (.q o))) port))
     (when (.collateral-blocked? o)
       (display " " port)
       (display '("collateral-blocked?" . "true") port))
