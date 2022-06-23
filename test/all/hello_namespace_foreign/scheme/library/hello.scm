c2770f1c031f67d63a4c3a8396c58596c0a058f0
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-06-23 09:22:30 +0200

    test: hello_namespace_foreign: Update scheme.
    * test/all/hello_namespace_foreign/scheme/library/hello.scm: Update.

--- test/all/hello_namespace_foreign/scheme/library/hello.scm
+++ test/all/hello_namespace_foreign/scheme/library/hello.scm
@@ -1,6 +1,6 @@
 ;;; Dezyne --- Dezyne command line tools
 ;;;
-;;; Copyright © 2020 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2020, 2022 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of Dezyne.
 ;;;
@@ -16,11 +16,10 @@
 ;;;
 ;;; You should have received a copy of the GNU Affero General Public
 ;;; License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
-;;;
 (define-module (library hello)
+  #:use-module (ice-9 control)
   #:use-module (oop goops)
   #:use-module (dzn runtime)
-
   #:duplicates (merge-generics)
   #:export (<library:ihello>
     <library:ihello.in>
@@ -31,63 +30,40 @@
     .h
     .w
     .b
-    h-hello
-    w-howdy
     <library:iworld>
     <library:iworld.in>
     <library:iworld.out>
     .world
-    .howdy)
-)
+    .howdy))
 
 (define true #t)
 (define false #f)
 
-
-
-
-
-
 (define-class <library:ihello.in> (<dzn:port>)
-  (hello #:accessor .hello #:init-keyword #:hello)
-
-)
+  (hello #:accessor .hello #:init-keyword #:hello))
 
 (define-class <library:ihello.out> (<dzn:port>)
-  (goodbye #:accessor .goodbye #:init-keyword #:goodbye)
+  (goodbye #:accessor .goodbye #:init-keyword #:goodbye))
 
-)
+(define-class <library:ihello> (<dzn:interface>))
 
-(define-class <library:ihello> (<dzn:interface>)
-)
 
 (define-class <library:iworld.in> (<dzn:port>)
-  (world #:accessor .world #:init-keyword #:world)
-
-)
+  (world #:accessor .world #:init-keyword #:world))
 
 (define-class <library:iworld.out> (<dzn:port>)
-  (howdy #:accessor .howdy #:init-keyword #:howdy)
+  (howdy #:accessor .howdy #:init-keyword #:howdy))
 
-)
+(define-class <library:iworld> (<dzn:interface>))
 
-(define-class <library:iworld> (<dzn:interface>)
-)
 ;; (use-modules (library:foreign))
 
 
 (define-class <library:hello> (<dzn:component>)
   (b #:accessor .b #:init-form true)
-
-
   (out_h #:accessor .out_h #:init-value #f #:init-keyword #:out_h)
-
-
   (h #:accessor .h #:init-form (make <library:ihello>) #:init-keyword #:h)
-
-  (w #:accessor .w #:init-form (make <library:iworld>) #:init-keyword #:w)
-
-)
+  (w #:accessor .w #:init-form (make <library:iworld>) #:init-keyword #:w))
 
 (define-method (initialize (o <library:hello>) args)
   (next-method o (cons* #:flushes? #t args))
@@ -96,50 +72,43 @@
       #:in (make <library:ihello.in>
         #:name "h"
         #:self o
-        #:hello (lambda args (call-in o (lambda _ (apply h-hello (cons o args))) `(,(.h o) "hello"))))
+        #:hello (lambda args
+          (call-in o
+            (lambda _
+              (apply h-hello (cons o args))
+              (dzn:flush o)
+              *unspecified*)
+            `(,(.h o) "hello"))))
       #:out (make <library:ihello.out>)))
-
   (set! (.w o)
     (make <library:iworld>
       #:in (make <library:iworld.in>)
       #:out (make <library:iworld.out>
         #:name "w"
         #:self o
-        #:howdy (lambda args (call-out o (lambda _ (apply w-howdy (cons o args))) `(,(.w o) "howdy"))))))
-
-
-)
-
+        #:howdy (lambda args
+          (call-out o
+            (lambda _
+              (apply w-howdy (cons o args))
+              (dzn:flush o)
+              *unspecified*)
+            `(,(.w o) "howdy")))))))
 
 (define-method (h-hello (o <library:hello>))
-
   (cond (true
-
       (let ()
         *unspecified*
         (set! (.b o) false)
-        (action o .w .in .world )
+        (action o .w .in .world )))
+    (false
+      (((compose .illegal .runtime) o)))))
 
-      )
-    )(false
-      (((compose .illegal .runtime) o))))
-  *unspecified*
-)
 (define-method (w-howdy (o <library:hello>))
-
   (cond ((not (.b o))
-
       (let ()
         *unspecified*
         (set! (.b o) true)
-        (action o .h .out .goodbye )
-
-      )
-    )((not (not (.b o)))
-      (((compose .illegal .runtime) o)))(else ((compose .illegal .runtime) o))
-  )
-  *unspecified*
-)
-
-
-;; code generator version: 0.0.91-1ed0
+        (action o .h .out .goodbye )))
+    ((.b o)
+      (((compose .illegal .runtime) o)))
+    (else ((compose .illegal .runtime) o))))
