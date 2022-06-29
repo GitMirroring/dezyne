0e28818a0503a0bcc8758d9b4afc751cb1e75973
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-06-29 20:57:27 +0200

    test: Add hello_foreign_out.
    * test/all/hello_foreign_out: New test.
    * test/all/local.mk (HELLO_TESTS): Add it.

---
+++ test/all/hello_foreign_out/c++/Foreign.hh
@@ -0,0 +1,48 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2018 Jan Nieuwenhuizen <janneke@gnu.org>
+// Copyright © 2019 Rutger van Beusekom <rutger@dezyne.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#ifndef FOREIGN_HH
+#define FOREIGN_HH
+
+#include <dzn/locator.hh>
+#include <dzn/runtime.hh>
+
+struct Foreign
+{
+  dzn::meta dzn_meta;
+  dzn::runtime& dzn_rt;
+  dzn::locator const& dzn_locator;
+  bool b;
+  std::function<void ()> out_w;
+  ::iworld w;
+  Foreign(const dzn::locator&);
+  friend std::ostream& operator << (std::ostream& os, const Foreign& m)  {
+    (void)m;
+    return os << "[" << m.b <<"]" ;
+  }
+  private:
+  void w_world();
+};
+
+#endif // FOREIGN_HH
--- test/all/local.mk
+++ test/all/local.mk
@@ -55,6 +55,7 @@ HELLO_TESTS =					\
  %D%/hello_external_inevitable			\
  %D%/hello_flush				\
  %D%/hello_foreign				\
+ %D%/hello_foreign_out				\
  %D%/hello_function				\
  %D%/hello_function_argument			\
  %D%/hello_function_void_bool_if		\
