7102ebd557e22440db87f41814ff11555ec317db
Rutger van Beusekom <rutger@dezyne.org>
2022-07-06 14:03:30 +0200

    test: Remove redundant compliance_failures_illegal.
    * test/all/compliance_failures_illegal: Remove test.
    * test/all/local.mk (REGRESSION_TESTS): Remove it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -188,7 +188,6 @@ REGRESSION_TESTS =				\
  %D%/compliance_failures_blocking_race		\
  %D%/compliance_failures_choice			\
  %D%/compliance_failures_defer			\
- %D%/compliance_failures_illegal		\
  %D%/compliance_failures_inevitable		\
  %D%/compliance_failures_inevitable_optional    \
  %D%/compliance_failures_multiple_provides	\
