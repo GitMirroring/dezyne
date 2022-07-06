5f4872446adaae31d02dd70f66c8aabd2f677bf8
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-07-07 00:11:46 +0200

    DRAFT test: Add complex_assign.
    * test/all/complex_assign/complex_assign.dzn: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -194,6 +194,7 @@ REGRESSION_TESTS =				\
  %D%/collateral_double_blocked_out		\
  %D%/comment_traceability			\
  %D%/complex_and				\
+ %D%/complex_assign				\
  %D%/complex_expressions			\
  %D%/complex_not				\
  %D%/complex_or					\
