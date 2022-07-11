03e07617c759e9e950a54104c9169941c66da251
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-07-11 07:18:18 +0200

    DRAFT test: Add complex_member.
    * test/all/complex_member/complex_member.dzn: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -198,6 +198,7 @@ REGRESSION_TESTS =				\
  %D%/complex_assign				\
  %D%/complex_expressions			\
  %D%/complex_int				\
+ %D%/complex_member				\
  %D%/complex_not				\
  %D%/complex_or					\
  %D%/compliance_async				\
