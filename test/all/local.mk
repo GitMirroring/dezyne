a687a6c17d706ec33002ad7a0b0e2435bf0daa1c
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-08-25 07:23:30 +0200

    test: Add hello_nondet.
    * test/all/hello_nondet/hello_nondet.dzn: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -326,6 +326,7 @@ REGRESSION_TESTS =				\
  %D%/hello_namespace_shadow			\
  %D%/hello_namespace_shadow2			\
  %D%/hello_no_compliance			\
+ %D%/hello_nondet				\
  %D%/hello_optional_flush			\
  %D%/hello_optional_nondet			\
  %D%/hello_out_data				\
