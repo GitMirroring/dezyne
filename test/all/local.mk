0eed02349a28e9acfa3301f7b33d2f6c9f8cb8ae
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-09-19 07:43:52 +0200

    test: Add constraint_nondet2.
    * test/all/constraint_nondet2: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -218,6 +218,7 @@ REGRESSION_TESTS =				\
  %D%/component_modeling_loop			\
  %D%/compound.space				\
  %D%/constraint_nondet				\
+ %D%/constraint_nondet2				\
  %D%/data_full					\
  %D%/deadlock_asynchronous_sync_reply		\
  %D%/deadlock_blocking_compliance		\
