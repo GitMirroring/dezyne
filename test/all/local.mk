6150518d90f00f190832fe848cd466b629f8e358
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-12-04 14:01:00 +0100

    test: range_assign_expression: Rename from range_expression.
    * test/all/range_expression: Rename to...
    * test/all/range_assign_expression: ...this, remove component and
    canonicalize.
    * test/all/local.mk (REGRESSION_TESTS): Update accordingly.
    * test/all/range_assign_expression/trace: Update accordingly.
    * test/all/range_assign_expression/baseline: Update baseline.

--- test/all/local.mk
+++ test/all/local.mk
@@ -413,7 +413,7 @@ REGRESSION_TESTS =				\
  %D%/range_action				\
  %D%/range_argument				\
  %D%/range_assign				\
- %D%/range_expression				\
+ %D%/range_assign_expression			\
  %D%/range_function				\
  %D%/range_local				\
  %D%/range_local_expression			\
