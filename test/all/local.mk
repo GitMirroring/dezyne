2fb07df5efc8ba99125f27538dbe31218e4bda9d
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2023-01-16 08:53:31 +0100

    test: Add compliance_system_hello_world.
    * test/all/compliance_system_hello_world: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -216,6 +216,7 @@ REGRESSION_TESTS =				\
  %D%/compliance_requires_illegal		\
  %D%/compliance_single_to_multiple		\
  %D%/compliance_sync_action			\
+ %D%/compliance_system_hello_world		\
  %D%/compliance_system_provides_bool		\
  %D%/component_modeling_loop			\
  %D%/compound.space				\
