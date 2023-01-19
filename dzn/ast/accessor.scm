2afb9e2621c1e4f03ef9886b15d393d1c6d34f9c
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2023-01-19 16:56:25 +0100

    ast: Fix data == $..$ crash well-formedness check.
    Reported by Michael van de Ven <michael.van.de.ven@thermofisher.com> via
    IRC.
    
    * dzn/ast/accessor.scm (ast:full-name<declaration>): Cater for non-tree
    element.

--- dzn/ast/accessor.scm
+++ dzn/ast/accessor.scm
@@ -1,6 +1,6 @@
 ;;; Dezyne --- Dezyne command line tools
 ;;;
-;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2014, 2018, 2020, 2021, 2022, 2023 Rutger van Beusekom <rutger@dezyne.org>
 ;;; Copyright © 2017, 2018, 2019, 2020 Rob Wieringa <rma.wieringa@gmail.com>
 ;;; Copyright © 2017, 2018, 2020 Johri van Eerd <vaneerd.johri@gmail.com>
@@ -189,9 +189,14 @@
   '("void"))
 
 (define-method (ast:full-name (o <declaration>))
-  (if (is-a? o <named>)
-      (append (ast:full-name (ast:parent o <scope>)) (list (ast:name o)))
-      (ast:full-name (ast:parent o <scope>))))
+  (let ((scope (ast:parent o <scope>)))
+    (cond
+     ((not scope)
+      #f)
+     ((is-a? o <named>)
+      (append (ast:full-name scope) (list (ast:name o))))
+     (else
+      (ast:full-name scope)))))
 
 (define-method (ast:full-name (o <root>))
   '())
