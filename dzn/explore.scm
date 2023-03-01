84058d61b06e912c2970a1062a59cf5c1862e7ef
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2023-03-01 07:44:56 +0100

    explore: Add <illegal-error> after <queue-full-error>.
    This makes (some) LTSs with a <queue-full> error equal again.
    
    * dzn/explore.scm (rtc-lts->lts)[fix-queue-full-trace]: New inner
    define.  Use it to add <illegal-error> after <queue-full-error>.
    
    * test/all/defer_queuefull/META,
    test/all/queuefull_external/META: Remove.

--- dzn/explore.scm
+++ dzn/explore.scm
@@ -545,6 +545,17 @@ RTC-LTS->STATE-DIAGRAM."
    ((from label to)
      ...))"
 
+  (define (fix-queue-full-trace trace)
+    (let* ((pc (car trace))
+           (status (.status pc)))
+      (if (not (is-a? status <queue-full-error>)) trace
+          (let* ((statement (.statement pc))
+                 (illegal (make <illegal-error>
+                            #:message "illegal"
+                            #:ast statement))
+                 (illegal-pc (clone pc #:status illegal)))
+            (cons illegal-pc trace)))))
+
   (define (fix-missing-reply-trace trace)
     (let* ((pc (car trace))
            (status (.status pc)))
@@ -572,6 +583,7 @@ numbers"
     "From a run-to-completion transition PC,TRACE, generate one LTS
 triple per label in the trail of the transition."
     (let* ((trace (fix-missing-reply-trace trace))
+           (trace (fix-queue-full-trace trace))
            (labels (parameterize ((%modeling? #t))
                      (map cdr (trace->trail trace))))
            (labels (if (null? labels) '("tau") labels))
