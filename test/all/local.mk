30a95fb06c4a55f185bc054194d661004670b26f
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2023-04-11 09:11:54 +0200

    test: Add hello_trigger_list.
    * test/all/hello_trigger_list/hello_trigger_list.dzn: New test.
    * test/all/local.mk (HELLO_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -89,6 +89,7 @@ HELLO_TESTS =					\
  %D%/hello_shared				\
  %D%/hello_system				\
  %D%/hello_system_reply				\
+ %D%/hello_trigger_list				\
  %D%/hello_two					\
  %D%/hello_usuk					\
  %D%/helloworld					\
