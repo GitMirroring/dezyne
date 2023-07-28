e27856442990091cfae1330f3905556b37284a49
Rutger van Beusekom <rutger@dezyne.org>
2023-07-28 18:58:39 +0200

    test: Add defer_after_call.
    * test/all/defer_after_call/defer_after_call.dzn: New model.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -249,6 +249,7 @@ REGRESSION_TESTS =				\
  %D%/deadlock_port_blocked			\
  %D%/deadlock_reply				\
  %D%/deadlock_reply_modeling			\
+ %D%/defer_after_call				\
  %D%/defer_blocking				\
  %D%/defer_cancel				\
  %D%/defer_cancel_call				\
