d51fc8f43a23cd758399e4d39b510e6ab591abcc
Rutger van Beusekom <rutger@dezyne.org>
2023-08-15 12:04:39 +0200

    test: Add shadow_shared_enum.
    * test/all/shadow_shared_enum/shadow_shared_enum.dzn: Add it.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -456,6 +456,7 @@ REGRESSION_TESTS =				\
  %D%/second_reply_sync				\
  %D%/second_reply_blocking			\
  %D%/shadow_port				\
+ %D%/shadow_shared_enum				\
  %D%/shared_assign				\
  %D%/shared_async_provides			\
  %D%/shared_async_requires			\
