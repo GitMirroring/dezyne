2ad1ab9d455014840f2aadab56164939fb6fee37
Rutger van Beusekom <rutger@dezyne.org>
2023-09-06 13:28:33 +0200

    runtime: c++: Use dzn::thread::defer in pump.
    It has been observed that std::async has arbitrary limits on some
    microsoft systems.  The dzn::thread::pool has no arbitrary limits,
    except for being limited by the available system resources.
    
    * runtime/c++/pump.cc (pump::pump): Replace std::async with
    dzn::thread::defer indirection that allows switching between std::async
    and the dzn::thread::pool.

--- runtime/c++/pump.cc
+++ runtime/c++/pump.cc
@@ -107,7 +107,7 @@ namespace dzn
   , paused(false)
   , current_coroutine(0)
   , switch_context()
-  , task(std::async(std::launch::async, std::ref(*this)))
+  , task(dzn::thread::defer(std::ref(*this)))
   {}
   pump::~pump()
   {
