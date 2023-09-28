49cd4ada05f8eb43b51992a4f84d9ada004c67ab
Rutger van Beusekom <rutger@dezyne.org>
2023-09-28 10:37:46 +0200

    test: shell_injected: Fix verification.
    * test/all/shell_injected/META: Enable verification; disable cs,
    scheme, lts.
    * test/all/shell_injected/shell_injected.dzn: Add a watchdog to armor
    against delayed refusal.
    * test/all/shell_injected/trace: Add watchdog events.
    * test/all/shell_injected/c++/Watchdog.hh,
    test/all/shell_injected/c++/main.cc: New files.

---
+++ test/all/shell_injected/c++/Watchdog.hh
@@ -0,0 +1,45 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2023 Rutger van Beusekom <rutger@dezyne.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+struct Watchdog: public skel::Watchdog
+{
+  dzn::pump& pump;
+  Watchdog (const dzn::locator& locator)
+    : skel::Watchdog (locator)
+    , pump (locator.get<dzn::pump> ())
+  {}
+  void w_set ()
+  {
+    pump.handle (reinterpret_cast<size_t> (this), 1000, [this]
+    {
+      std::cout << "BEFORE TIMEOUT" << std::endl;
+      this->w_timeout ();
+      std::cout << "AFTER TIMEOUT" << std::endl;
+    });
+    std::cout << "TIMERSET" << std::endl;
+  }
+  void w_cancel ()
+  {
+    pump.remove (reinterpret_cast<size_t> (this));
+  }
+};
---
+++ test/all/shell_injected/c++/main.cc
@@ -0,0 +1,46 @@
+// Dezyne --- Dezyne command line tools
+//
+// Copyright © 2023 Rutger van Beusekom <rutger@dezyne.org>
+//
+// This file is part of Dezyne.
+//
+// Dezyne is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Dezyne is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with Dezyne.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Commentary:
+//
+// Code:
+
+#include "shell_injected.hh"
+
+#include <dzn/locator.hh>
+#include <dzn/runtime.hh>
+
+int
+main ()
+{
+  dzn::locator locator;
+  dzn::runtime runtime;
+  shell_injected sut (locator.set (runtime));
+
+  sut.p.out.f = []{};
+  sut.r.in.e = []{};
+
+  std::thread t ([&]
+  {
+    std::this_thread::sleep_for (std::chrono::milliseconds (50));
+    sut.r.out.f ();
+  });
+  sut.p.in.e ();
+  t.join ();
+}
