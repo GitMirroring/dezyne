0137da6b4fd41076338e6776fb255788469e7be0
Rutger van Beusekom <rutger@dezyne.org>
2024-01-09 12:08:28 +0100

    test: Add inner_guard_shared_state_deadlock.
    * test/all/inner_guard_shared_state_deadlock: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -1,7 +1,7 @@
 # Dezyne --- Dezyne command line tools
 #
 # Copyright © 2019, 2020, 2021, 2022, 2023, 2024 Janneke Nieuwenhuizen <janneke@gnu.org>
-# Copyright © 2020, 2021, 2022, 2023 Rutger van Beusekom <rutger@dezyne.org>
+# Copyright © 2020, 2021, 2022, 2023, 2024 Rutger van Beusekom <rutger@dezyne.org>
 # Copyright © 2020, 2021, 2022, 2023 Paul Hoogendijk <paul@dezyne.org>
 # Copyright © 2020 Johri van Eerd <vaneerd.johri@gmail.com>
 # Copyright © 2020 Rob Wieringa <rma.wieringa@gmail.com>
@@ -426,6 +426,7 @@ REGRESSION_TESTS =				\
  %D%/inevitable_performance			\
  %D%/injected_dangling				\
  %D%/injected_foreign				\
+ %D%/inner_guard_shared_state_deadlock		\
  %D%/inner.space				\
  %D%/integer_expressions			\
  %D%/interface_enum				\
