b5d52764fc03a54d9ae23e1effda48a0dc206d47
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-05-27 11:49:44 +0200

    test: Add hello_choice_requires.
    * test/all/hello_choice_requires: New test.
    * test/all/local.mk (SEMANTIC_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -51,6 +51,7 @@ SEMANTIC_TESTS =				\
  %D%/hello_block				\
  %D%/hello_block_requires			\
  %D%/hello_choice				\
+ %D%/hello_choice_requires			\
  %D%/hello_cruelworld_requires			\
  %D%/hello_reply_bool				\
  %D%/hello_reply_bool_requires			\
