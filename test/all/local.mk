248127891b003ae7f21e6a454c6fc3ca72ee9ab0
Rutger van Beusekom <rutger@dezyne.org>
2024-05-29 12:33:13 +0200

    test: Add ihello_cruel_world.
    * test/all/ihello_cruel_world: New test.
    * test/all/local.mk (SEMANTIC_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -40,6 +40,7 @@ SEMANTIC_TESTS =				\
  %D%/ihello__world				\
  %D%/ihello_choice				\
  %D%/ihello_cruel__world			\
+ %D%/ihello_cruel_world				\
  %D%/ihello_cruelworld				\
  %D%/ihello_i_world				\
  %D%/ihello_optional_choice			\
