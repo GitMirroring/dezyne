55a1e0ccdc455f5445649259a925fe97bb20bec4
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-06-20 14:03:20 +0200

    test: system_shared_choice2: Rename from system_shared.
    * test/all/system_shared: Rename to...
    * test/all/system_shared_choice2: ...this.
    * test/all/shared_choice2/shared.dzn: New symlink.
    * test/all/system_shared_choice2/baseline: Update accordingly.

--- test/all/local.mk
+++ test/all/local.mk
@@ -626,7 +626,7 @@ REGRESSION_TESTS =				\
  %D%/system_out_two				\
  %D%/system_reply_bool				\
  %D%/system_reply_enum				\
- %D%/system_shared				\
+ %D%/system_shared_choice2			\
  %D%/system_shared_defer			\
  %D%/unreachable_constraint			\
  %D%/unreachable_crosstalk			\
