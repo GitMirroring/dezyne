607c0dc23578ad698a3ca5dfbac2e174c5daefd5
Rutger van Beusekom <rutger@dezyne.org>
2024-08-13 14:06:45 +0200

    ast: Make ast:modeling? robust against name lookup failure.
    * dzn/ast.scm (ast:modeling?): Handle .event returning #f.

--- dzn/ast.scm
+++ dzn/ast.scm
@@ -1,7 +1,7 @@
 ;;; Dezyne --- Dezyne command line tools
 ;;;
 ;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024 Janneke Nieuwenhuizen <janneke@gnu.org>
-;;; Copyright © 2014, 2018, 2020, 2021, 2022, 2023 Rutger van Beusekom <rutger@dezyne.org>
+;;; Copyright © 2014, 2018, 2020, 2021, 2022, 2023, 2024 Rutger van Beusekom <rutger@dezyne.org>
 ;;; Copyright © 2017, 2018, 2019, 2020 Rob Wieringa <rma.wieringa@gmail.com>
 ;;; Copyright © 2017, 2018, 2020 Johri van Eerd <vaneerd.johri@gmail.com>
 ;;; Copyright © 2018 Filip Toman <filip.toman@verum.com>
@@ -282,7 +282,7 @@
   #t)
 
 (define-method (ast:modeling? (o <trigger>))
-  ((compose ast:modeling? .event) o))
+  ((compose (cute and=> <> ast:modeling?) .event) o))
 
 (define-method (ast:optional? (o <optional>))
   #t)
