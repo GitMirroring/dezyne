0888aa64794c6e553005eb0348079202baccb455
Rutger van Beusekom <rutger@dezyne.org>
2024-08-13 13:24:42 +0200

    test: Add foreign_trivial_integration.
    * test/all/foreign_trivial_integration: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -353,6 +353,7 @@ REGRESSION_TESTS =				\
  %D%/foreign_optional				\
  %D%/foreign_reply				\
  %D%/foreign_requires				\
+ %D%/foreign_trivial_integration		\
  %D%/formal_binding				\
  %D%/function_early_return			\
  %D%/function_reply_early_return		\
