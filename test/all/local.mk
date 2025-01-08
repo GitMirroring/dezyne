6df1a6b61434c8fa76a390b972f324a5b4e73275
Paul Hoogendijk <paul@dezyne.org>
2025-01-08 09:23:53 +0100

    test: Add invariant_blocking_two_out.
    * test/all/invariant_blocking_two_out: New test.
    * test/all/local.mk (REGRESSION_TESTS): Register it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -548,6 +548,7 @@ REGRESSION_TESTS =				\
  %D%/interface_enum				\
  %D%/interface_int				\
  %D%/interface_missing_reply			\
+ %D%/invariant_blocking_two_out			\
  %D%/invariant_sync_out				\
  %D%/iteration					\
  %D%/label_instance_mismatch			\
