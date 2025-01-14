267411330e49edf2445c541bb3e87956745fe09a
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-14 09:09:13 +0100

    language: Merge gratuitous inner let.
    * dzn/commands/language.scm (main): Collapse inner context let.

--- dzn/commands/language.scm
+++ dzn/commands/language.scm
@@ -133,48 +133,48 @@ Dezyne language tool for completion and lookup information
                             (lambda (line col)
                               (line-column->offset line col input)))))
                        (car parse-result)))
-           (parse-tree (cdr parse-result)))
-      (let ((context (complete:context parse-tree offset)))
-        (when (> debugity 2)
-          (display "input:\n" (current-error-port))
-          (display input (current-error-port))
-          (display "parse-tree:\n" (current-error-port))
-          (pretty-print parse-tree (current-error-port)))
-        (when (> debugity 1)
-          (display "context:\n" (current-error-port))
-          (pretty-print context (current-error-port)))
-        (when debug?
-          (format (current-error-port) "offset:~a\n" offset)
-          (format (current-error-port) "tree:~s\n" (.tree context)))
-        (cond
-         (stress?
-          (stress file-name #:debug? debug?))
-         (lookup?
-          (let* ((token (.tree context))
-                 (def   (lookup-definition
-                         token context
-                         #:file-name file-name
-                         #:file-name->parse-tree file-name->parse-tree
-                         #:resolve-file resolve-file)))
-            (when (> debugity 0)
-              (display "definition:\n" (current-error-port))
-              (pretty-print def (current-error-port)))
-            (when verbose?
-              (display "location:\n"))
-            (unless def
-              (when verbose?
-                (display "not found\n"))
-              (exit EXIT_FAILURE))
-            (let* ((file-name (or (tree:file-name def) file-name))
-                   (text   (file-name->text file-name))
-                   (target (or (tree:name def) def))
-                   (loc    (tree:->location target text)))
-              (display (location->string loc))
-              (newline))))
-         (else
+           (parse-tree (cdr parse-result))
+           (context (complete:context parse-tree offset)))
+      (when (> debugity 2)
+        (display "input:\n" (current-error-port))
+        (display input (current-error-port))
+        (display "parse-tree:\n" (current-error-port))
+        (pretty-print parse-tree (current-error-port)))
+      (when (> debugity 1)
+        (display "context:\n" (current-error-port))
+        (pretty-print context (current-error-port)))
+      (when debug?
+        (format (current-error-port) "offset:~a\n" offset)
+        (format (current-error-port) "tree:~s\n" (.tree context)))
+      (cond
+       (stress?
+        (stress file-name #:debug? debug?))
+       (lookup?
+        (let* ((token (.tree context))
+               (def   (lookup-definition
+                       token context
+                       #:file-name file-name
+                       #:file-name->parse-tree file-name->parse-tree
+                       #:resolve-file resolve-file)))
+          (when (> debugity 0)
+            (display "definition:\n" (current-error-port))
+            (pretty-print def (current-error-port)))
           (when verbose?
-            (display "completions:\n"))
-          (pretty-print (complete (.tree context) context offset
-                                  #:debug? debug?
-                                  #:file-name->parse-tree file-name->parse-tree
-                                  #:imports imports))))))))
+            (display "location:\n"))
+          (unless def
+            (when verbose?
+              (display "not found\n"))
+            (exit EXIT_FAILURE))
+          (let* ((file-name (or (tree:file-name def) file-name))
+                 (text   (file-name->text file-name))
+                 (target (or (tree:name def) def))
+                 (loc    (tree:->location target text)))
+            (display (location->string loc))
+            (newline))))
+       (else
+        (when verbose?
+          (display "completions:\n"))
+        (pretty-print (complete (.tree context) context offset
+                                #:debug? debug?
+                                #:file-name->parse-tree file-name->parse-tree
+                                #:imports imports)))))))
