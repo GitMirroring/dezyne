c8bf58836460ef3f81205291f2f29ac75dd14f13
Rutger van Beusekom <rutger@dezyne.org>
2025-05-08 16:50:10 +0200

    test: Camera: Update foreign for new enum usage.
    * test/all/Camera/c++/Contrast.hh (struct Contrast): Remove old style
    ::type.

--- test/all/Camera/c++/Contrast.hh
+++ test/all/Camera/c++/Contrast.hh
@@ -25,7 +25,7 @@ struct Contrast: public skel::Contrast
   Contrast (const dzn::locator &l)
     : skel::Contrast (l)
   {}
-  virtual ::IContrast::EContrast::type port_measure () {std::clog << "measure\n"; return ::IContrast::EContrast::Blurrier;}
+  virtual ::IContrast::EContrast port_measure () {std::clog << "measure\n"; return ::IContrast::EContrast::Blurrier;}
 };
 
 #endif // CONTRAST_HH
