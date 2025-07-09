ad7f6842d275d8fc8e47f86c32fcaa000cb0af0a
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-07-09 14:26:42 +0200

    build-auix/indent.scm: Fixes for Emacs 30.

--- build-aux/indent.scm
+++ build-aux/indent.scm
@@ -5,7 +5,7 @@ exec guile -L ${0%/*} -e '(indent)' -s "$0" "$@"
 
 ;;; Dezyne --- Dezyne command line tools
 ;;;
-;;; Copyright © 2023 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2023, 2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of Dezyne.
 ;;;
@@ -46,17 +46,100 @@ exec guile -L ${0%/*} -e '(indent)' -s "$0" "$@"
    "--no-init-file"
    "--eval"
    (format #f "
-(let ((error nil)
-      (version-control nil))
+(progn
+  (defun vc-mode-line-state (&rest ignored) nil)
+  (setq error nil)
+  (setq version-control nil)
+  (setq vc-use-short-revision nil)
+  (setq scheme-mode-hook nil)
   (setq enable-local-eval :all)
   (setq enable-local-variables :all)
   (load-library \"scheme\")
   (find-file ~s)
   (scheme-mode)
+
+  ;; Guix' dir-locals
+  (put 'lambda* 'scheme-indent-function 1)
+  (put 'test-assert 'scheme-indent-function 1)
+  (put 'test-assertm 'scheme-indent-function 1)
+  (put 'test-equalm 'scheme-indent-function 1)
+  (put 'test-equal 'scheme-indent-function 1)
+  (put 'test-eq 'scheme-indent-function 1)
+  (put 'call-with-input-string 'scheme-indent-function 1)
+  (put 'call-with-port 'scheme-indent-function 1)
+
+  ;; geiser-syntax--scheme-indent -- geiser-syntax.el
+  (put 'and-let* 'scheme-indent-function 1)
+  (put 'case-lambda 'scheme-indent-function 0)
+  (put 'catch 'scheme-indent-function 'defun)
+  (put 'class 'scheme-indent-function 'defun)
+  (put 'dynamic-wind 'scheme-indent-function 0)
+  (put 'guard 'scheme-indent-function 1)
+  (put 'let*-values 'scheme-indent-function 1)
+  (put 'let-values 'scheme-indent-function 1)
+  (put 'let/ec 'scheme-indent-function 1)
+  (put 'letrec* 'scheme-indent-function 1)
+  (put 'match 'scheme-indent-function 1)
+  (put 'match-lambda 'scheme-indent-function 0)
+  (put 'match-lambda* 'scheme-indent-function 0)
+  (put 'match-let 'scheme-indent-function 'scheme-let-indent)
+  (put 'match-let* 'scheme-indent-function 1)
+  (put 'match-letrec 'scheme-indent-function 1)
+  (put 'opt-lambda 'scheme-indent-function 1)
+  (put 'parameterize 'scheme-indent-function 1)
+  (put 'parameterize* 'scheme-indent-function 1)
+  (put 'receive 'scheme-indent-function 2)
+  (put 'require-extension 'scheme-indent-function 0)
+  (put 'syntax-case 'scheme-indent-function 2)
+  (put 'test-approximate 'scheme-indent-function 1)
+  (put 'test-assert 'scheme-indent-function 1)
+  (put 'test-eq 'scheme-indent-function 1)
+  (put 'test-equal 'scheme-indent-function 1)
+  (put 'test-eqv 'scheme-indent-function 1)
+  (put 'test-group 'scheme-indent-function 1)
+  (put 'test-group-with-cleanup 'scheme-indent-function 1)
+  (put 'test-runner-on-bad-count! 'scheme-indent-function 1)
+  (put 'test-runner-on-bad-end-name! 'scheme-indent-function 1)
+  (put 'test-runner-on-final! 'scheme-indent-function 1)
+  (put 'test-runner-on-group-begin! 'scheme-indent-function 1)
+  (put 'test-runner-on-group-end! 'scheme-indent-function 1)
+  (put 'test-runner-on-test-begin! 'scheme-indent-function 1)
+  (put 'test-runner-on-test-end! 'scheme-indent-function 1)
+  (put 'test-with-runner 'scheme-indent-function 1)
+  (put 'unless 'scheme-indent-function 1)
+  (put 'when 'scheme-indent-function 1)
+  (put 'while 'scheme-indent-function 1)
+  (put 'with-exception-handler 'scheme-indent-function 1)
+  (put 'with-syntax 'scheme-indent-function 1)
+
+  ;; geiser-syntax--scheme-indent -- geiser-guile.el
+  (put 'c-declare 'scheme-indent-function 0)
+  (put 'c-lambda 'scheme-indent-function 2)
+  (put 'call-with-input-string 'scheme-indent-function 1)
+  (put 'call-with-output-string 'scheme-indent-function 0)
+  (put 'call-with-prompt 'scheme-indent-function 1)
+  (put 'call-with-trace 'scheme-indent-function 0)
+  (put 'eval-when 'scheme-indent-function 1)
+  (put 'lambda* 'scheme-indent-function 1)
+  (put 'pmatch 'scheme-indent-function 'defun)
+  (put 'sigaction 'scheme-indent-function 1)
+  (put 'syntax-parameterize 'scheme-indent-function 1)
+  (put 'with-error-to-file 'scheme-indent-function 1)
+  (put 'with-error-to-port 'scheme-indent-function 1)
+  (put 'with-error-to-string 'scheme-indent-function 0)
+  (put 'with-fluid* 'scheme-indent-function 1)
+  (put 'with-fluids 'scheme-indent-function 1)
+  (put 'with-fluids* 'scheme-indent-function 1)
+  (put 'with-input-from-string 'scheme-indent-function 1)
+  (put 'with-method 'scheme-indent-function 1)
+  (put 'with-mutex 'scheme-indent-function 1)
+  (put 'with-output-to-string 'scheme-indent-function 0)
+  (put 'with-throw-handler 'scheme-indent-function 1)
+
   (indent-region (point-min) (point-max))
   (delete-trailing-whitespace)
   (when (buffer-modified-p (current-buffer))
-    (save-buffer)))"
+    (save-buffer))))"
            file)))
 
 (define (emacs-indent-c++ file)
@@ -66,8 +149,11 @@ exec guile -L ${0%/*} -e '(indent)' -s "$0" "$@"
    "--no-init-file"
    "--eval"
    (format #f "
-(let ((error nil)
-      (version-control nil))
+(progn
+  (defun vc-mode-line-state (&rest ignored) nil)
+  (setq error nil)
+  (setq version-control nil)
+  (setq vc-use-short-revision nil)
   (setq enable-local-eval :all)
   (setq enable-local-variables :all)
   (load-library \"cc-mode\")
