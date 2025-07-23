ed4953010a76095c51601ce183f080248caa215d
Paul Hoogendijk <paul@dezyne.org>
2025-07-23 14:42:16 +0200

    cs: Partial support replying <extern> data.
    * dzn/code/scmackerel/cs.scm: (ast->code <reply>): Use type of
    trigger for reply ($..$).

--- dzn/code/scmackerel/cs.scm
+++ dzn/code/scmackerel/cs.scm
@@ -2,6 +2,7 @@
 ;;;
 ;;; Copyright © 2022, 2023, 2024, 2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2025 Rutger (regtur) van Beusekom <rutger@dezyne.org>
+;;; Copyright © 2025 Paul Hoogendijk <paul@dezyne.org>
 ;;;
 ;;; This file is part of Dezyne.
 ;;;
@@ -169,6 +170,9 @@
       (ast->code (code:wrap-compound o)))
      (else
       (let* ((type (ast:type o))
+             (type (if (not (is-a? (.expression o) <data>)) type
+                       ((compose .type .signature .event car ast:trigger*)
+                        (ast:parent o <on>))))
              (reply-port (.port o))
              (port-name (.name reply-port))
              (out-binding (string-append (%member-prefix)
