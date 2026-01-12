7993f046bc97de77ca64d9f608884abc3bd6beb5
Janneke Nieuwenhuizen <janneke@gnu.org>
2026-01-12 21:18:24 +0100

    test: Add foreign_function_space_function.
    * test/all/foreign_function_space_function: New test.
    * test/all/local.mk (REGRESSION_TESTS): Add it.

--- test/all/local.mk
+++ test/all/local.mk
@@ -442,6 +442,7 @@ REGRESSION_TESTS =				\
  %D%/failures_second_inevitable			\
  %D%/flush_inevitable_sync_out			\
  %D%/foreign_function_shadow_event		\
+ %D%/foreign_function_space_function		\
  %D%/foreign_handwritten			\
  %D%/foreign_import				\
  %D%/foreign_import_system			\
